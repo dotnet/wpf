1f6dad4fa4657e9a66190c25ae950f9fc24efb29
Connor Lee <connorlee@u.boisestate.edu>
2018-12-04 18:49:48 -0700

    Add missing licenses for tests, and remove old headers (#79)

--- src/Microsoft.DotNet.Wpf/test/Common/TestServices/MS/Win32/NativeMethodsSetLastError.cs
+++ src/Microsoft.DotNet.Wpf/test/Common/TestServices/MS/Win32/NativeMethodsSetLastError.cs
@@ -1,12 +1,9 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
 //---------------------------------------------------------------------------
-//
-// <copyright file="NativeMethodsSetLastError.cs" company="Microsoft">
-//    Copyright (C) Microsoft Corporation.  All rights reserved.
-// </copyright>
-//
-//
 // Description: P/Invokes for methods that need to call SetLastError(0)
-//
 //---------------------------------------------------------------------------
 
 // The NativeMethodsSetLastError class differs between assemblies and could not actually be

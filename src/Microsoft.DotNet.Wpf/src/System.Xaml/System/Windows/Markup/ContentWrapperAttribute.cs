ea896157196309825610bb63c293d8d1c36d4db1
Hugh Bellamy <hughbellars@gmail.com>
2018-12-26 17:45:55 +0000

    Cleanup ContentWrapperAttribute

--- src/Microsoft.DotNet.Wpf/src/System.Xaml/System/Windows/Markup/ContentWrapperAttribute.cs
+++ src/Microsoft.DotNet.Wpf/src/System.Xaml/System/Windows/Markup/ContentWrapperAttribute.cs
@@ -2,17 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-//
-//  Microsoft Windows Client Platform
-//
-//
-//  Description: Can be specified on a collection type to indicate which 
-//               types are used to wrap content foreign content such as 
-//               strings in a strongly type Collection<T>. 
-//
-//  Created:     06/07/2005 Microsoft
-//
-
 using System.Runtime.CompilerServices;
 
 namespace System.Windows.Markup
@@ -24,7 +13,7 @@ namespace System.Windows.Markup
     /// </summary>
     [AttributeUsage(AttributeTargets.Class, AllowMultiple = true, Inherited = true)]
     [TypeForwardedFrom("WindowsBase, Version=4.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35")]
-    sealed public class ContentWrapperAttribute : Attribute
+    public sealed class ContentWrapperAttribute : Attribute
     {
         /// <summary>
         /// Declares the given type as being a content wrapper for the collection 
@@ -48,22 +37,19 @@ namespace System.Windows.Markup
         public override object TypeId => this;
 
         /// <summary>
-        ///     Overrides Object.Equals to implement correct equality semantics for this
-        ///     attribute.
+        /// Overrides Object.Equals to implement correct equality semantics for this
+        /// attribute.
         /// </summary>
         public override bool Equals(object obj)
         {
-            var other = obj as ContentWrapperAttribute;
-            if (other == null) return false;
-            return ContentWrapper == other.ContentWrapper;
+            return
+                obj is ContentWrapperAttribute other &&
+                ContentWrapper == other.ContentWrapper;
         }
 
         /// <summary>
-        ///     Overrides Object.GetHashCode to implement correct hashing semantics.
+        /// Overrides Object.GetHashCode to implement correct hashing semantics.
         /// </summary>
-        public override int GetHashCode()
-        {
-            return ContentWrapper.GetHashCode();
-        }
+        public override int GetHashCode() => ContentWrapper.GetHashCode();
     }
 }

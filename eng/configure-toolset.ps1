083719d2cc7e169148b94a148363584095bb0f6f
Vatsan Madhavan <vatsan.madhavan@microsoft.com>
2019-02-18 14:58:54 -0800

    Integrate changes needed to use Microsoft.Dotnet.Arcade.Wpf.Sdk

---
+++ eng/configure-toolset.ps1
@@ -0,0 +1,12 @@
+#
+# This file should be kept in sync across https://www.github.com/dotnet/wpf and dotnet-wpf-int repos. 
+#
+
+# This repo uses C++/CLI /clr:pure (or /clr:netcore) switches during compilation, which are 
+# deprecated. Ensure that this warning is always suppressed during build.
+if (($properties -eq $null) -or (-not ($properties -icontains '/nowarn:D9035'))) {
+    $properties = @('/nowarn:D9035') + $properties
+}
+
+# Always generate binary logs
+$binaryLog = $true

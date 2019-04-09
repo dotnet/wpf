4861d17da6ff4fda347dce3577aeef45a1114992
Steven Kirbach <steven.kirbach@gmail.com>
2019-04-09 11:10:11 -0700

    adding helper script and updating documentation

--- Documentation/developer-guide.md
+++ Documentation/developer-guide.md
@@ -23,6 +23,52 @@ We use the following workflow for building and testing features and fixes.
 
 You first need to [Fork](https://github.com/dotnet/corefx/wiki/Checking-out-the-code-repository#fork-the-repository) and [Clone](https://github.com/dotnet/corefx/wiki/Checking-out-the-code-repository#clone-the-repository) this WPF repository. This is a one-time task.
 
+### Testing Locally built WPF assemblies (excluding PresentationBuildTasks)
+This section of guide is intended to discuss the different approaches for ad-hoc testing of WPF assemblies, and not automated testing.
+There are a few different ways this can be done, and for the most part, it can be a matter of personal preference on which workflow you choose.
+
+#### Copying binaries to publish location of a self-contained application
+The simplest approach is to publish your sample app using `dotnet publish -r <rid> --self-contained`. You can add the `<SelfContained>true</SelfContained>`
+and `<RuntimeIdentifer>rid</RuntimeIdentifier>` properties to your .csproj or .vbproj file and then you can simply execute `dotnet publish`.
+We recommend always supplying a runtime identifier, as many of the WPF assemblies are architecture dependent. The values you can choose here
+are `win-x86` or `win-x64`.
+
+Then to copy the WPF assemblies to this published location, simply run the copy-wpf.ps1 script located in the `eng` folder of the repo and
+point it to the location of your test application:
+> eng\copy-wpf.ps1 -destination "proj location"
+
+#### Copying binaries to shared installation
+*Note: We recommend backing up the original files when doing this.*
+
+If you want/need to test an existing application that targets the shared installation, you can copy the assemblies directly over the existing Microsoft.WindowsDesktop.App ones. You can run the copy-wpf.ps1 script again, except this time the destination refers to the version 
+of the shared framework to copy over, and the `-shared` parameter must be passed in as well:
+> eng\copy-wpf.ps1 -destination "version" -shared  
+
+#### Testing API changes 
+The above instructions imply that you are testing assemblies that don't have any changes to the public API surface that the test application
+needs to use. If you need to test some API changes you are making, the C# team has done some great work to make this relatively straightforward.
+When the C# compiler detects a collision with assembly references, the assembly with the higher version number is chosen. Assuming our locally built
+binaries are newer than what is installed, we can then simply reference those local binaries directly from the project file, like this:
+
+*Note: you should build locally with the `-pack` param to ensure the binaries are put in the correct location.* 
+
+```
+  <PropertyGroup>
+     <!-- Change this value based on where your local repo is located -->
+     <WpfRepoRoot>d:\dev\src\dotnet\wpf</WpfRepoRoot>
+     <!-- Publishing a self-contained app ensures our binaries are used. -->
+     <SelfContained>true</SelfContained>
+    <!-- The runtime identifier here should match below. -->
+    <RuntimeIdentifier>win-x86</RuntimeIdentifier>
+  </PropertyGroup>
+  <ItemGroup>
+    <Reference Include="$(WpfRepoRoot)\artifacts\Microsoft.DotNet.Wpf.GitHub\ref\netcoreapp3.0\*.dll" Private="false" />
+    <ReferenceCopyLocalPaths Include="$(WpfRepoRoot)\artifacts\Microsoft.DotNet.Wpf.GitHub\lib\netcoreapp3.0\*.dll" />
+    <ReferenceCopyLocalPaths Include="$(WpfRepoRoot)\artifacts\Microsoft.DotNet.Wpf.GitHub\lib\win-x86\*.dll" />
+  </ItemGroup>
+```
+
+### Testing PresentationBuildTasks
 -- add more content here --
 
 ## More Information
---
+++ eng/copy-wpf.ps1
@@ -0,0 +1,117 @@
+# Copy wpf binaries from local build to desired location. The location can either be the version number (for copying to shared installation)
+# or the location of the project file if copying binaries to the output of a locally built application.
+Param(
+[Parameter(Mandatory=$true)]
+[string]$destination,
+[string]$arch="x86",
+[switch]$release,
+[switch]$shared,
+[switch]$help
+)
+
+function CopyByBinaryToPublishLocation($publishLocation)
+{
+    $locallyBuiltBinaryLocationBase = Join-Path $RepoRoot "artifacts\bin"
+    CopyNativeBinariesToPublishLocation $publishLocation $locallyBuiltBinaryLocationBase 
+    CopyManagedBinariesToPublishLocation $publishLocation  $locallyBuiltBinaryLocationBase
+    CopyThemeBinariesToPublishLocation $publishLocation  $locallyBuiltBinaryLocationBase
+    CopyUIAutomationBinariesToPublishLocation $publishLocation  $locallyBuiltBinaryLocationBase
+}
+
+function CopyNativeBinariesToPublishLocation($publishLocation, $localBinLocation)
+{
+    $NativeBinaries = "D3DCompiler", "PenImc", "PresentationNative", "wpfgfx"
+    $ArchFolder = if ($arch -eq "x86") { "Win32" } else { "x64" }
+    foreach($binary in $NativeBinaries)
+    {
+        $BinLocation = Join-Path (Join-Path (Join-Path $localBinLocation $binary) $ArchFolder) $Config
+        if ($binary -eq "D3DCompiler")
+        {
+            $binary = $binary + "_47"
+        }
+        $NetCore3NativeBinaryName = $binary + "_cor3.dll"
+        CopyBinaryToLocation $NetCore3NativeBinaryName $BinLocation $publishLocation
+    }
+}
+
+function CopyManagedBinariesToPublishLocation($publishLocation, $localBinLocation)
+{
+    # x86 managed binaries don't have a distinct folder
+    $ManagedBinaries = "DirectWriteForwarder", "PresentationCore", "PresentationCore-CommonResources", "PresentationUI", "PresentationFramework-SystemCore", "PresentationFramework-SystemData", "PresentationFramework-SystemXml", "PresenationFramework-SystemXmlLinq", "ReachFramework", "System.Printing", "System.Windows.Controls.Ribbon", "System.Windows.Input.Manipulations", "WindowsBase", "WindowsFormsIntegration"
+    CopyNetCoreApp3Binaries $publishLocation $localBinLocation $ManagedBinaries
+}
+
+function CopyThemeBinariesToPublishLocation($publishLocation, $localBinLocation)
+{
+    $ThemeBinaries = "PresentationFramework.Aero", "PresentationFramework.Aero2", "PresentationFramework.AeroLight", "PresentationFramework.Classic", "PresentationFramework.Luna", "PresentationFramework.Royale"
+    CopyNetCoreApp3Binaries $publishLocation $localBinLocation $ThemeBinaries
+}
+
+function CopyUIAutomationBinariesToPublishLocation($publishLocation, $localBinLocation)
+{
+    $UIAutomationBinaries = "UIAutomationClient", "UIAutomationClientSideProviders", "UIAutomationProvider", "UIAutomationTypes"
+    CopyNetCoreApp3Binaries $publishLocation $localBinLocation $UIAutomationBinaries
+}
+
+function CopyNetCoreApp3Binaries($publishLocation, $localBinLocation, $binaries)
+{
+    $ArchFolder = if ($arch -eq "x86") { "" } else { "x64" }
+    foreach($binary in $binaries)
+    {
+        $FullBinaryName = $binary + ".dll"
+        $BinLocation = Join-Path (Join-Path (Join-Path (Join-Path $localBinLocation $binary) $ArchFolder) $Config) "netcoreapp3.0"
+        CopyBinaryToLocation $FullBinaryName $BinLocation $publishLocation
+    }
+}
+
+function CopyBinaryToLocation($binary, $binaryLocation, $destination)
+{
+    $FullBinaryPath = Join-Path $binaryLocation $binary
+    if (Test-Path $FullBinaryPath)
+    {
+        Write-Host "Copying " $binary " to " $destination  
+        Copy-Item $FullBinaryPath $destination -Force
+    }
+    else
+    {
+        Write-Host "Binary Unavailable: " $binary
+    }
+}
+
+function Print-Usage()
+{
+    Write-Host "Usage: copy-wpf.ps1 -destination <value> [-arch <value] [-release] [-shared]"
+    Write-Host "    This script helps developers deploy wpf assemblies to the proper location for easy testing. See "
+    Write-Host "    developer-guide.md for more information on how to use this script."
+    Write-Host ""
+    Write-Host "Common parameters:"
+    Write-Host "  -destination <value>    Location of .csproj or .vbproj of application to test against. If copying"
+    Write-Host "                          over a shared installation, the value should be the version to copy over."
+    Write-Host "  -arch <value>           Architecture of binaries to copy. Can be either x64 or x86. Default is x86."
+    Write-Host "  -release                Copy release binaries. Default is to copy Debug binaries"
+    Write-Host "  -shared                 Copy binaries over the shared installation of Microsoft.WindowsDesktop.App."
+    Write-Host "  -help                   Print help and exit"
+    Write-Host ""
+}
+
+$RepoRoot = Resolve-Path (Join-Path $PSScriptRoot "..")
+$Config = if ($release) { "Release" } else { "Debug" }
+
+if ($help) 
+{
+    Print-Usage
+}
+elseif($shared)
+{
+    Write-Host "Copying binaries to shared installation"
+    $sharedLocation = Join-Path (Join-Path $env:ProgramFiles "dotnet\shared\Microsoft.WindowsDesktop.App") $destination
+    CopyByBinaryToPublishLocation $sharedLocation
+}
+else
+{
+    $runtimeIdentifer = if ($arch -eq "x86") { "win-x86" } else { "win-x64" }
+    $publishLocation = Join-Path (Join-Path (Join-Path $destination "bin\Debug\netcoreapp3.0") $runtimeIdentifer) "publish"
+    Write-Host "Copying binaries to app published directory"
+    CopyByBinaryToPublishLocation $publishLocation  
+}
+

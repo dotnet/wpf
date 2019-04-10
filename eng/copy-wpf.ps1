1cd5b32c0784ea383d74d38aa618324acae06824
Steven Kirbach <steven.kirbach@gmail.com>
2019-04-10 13:42:24 -0700

    fix script

--- eng/copy-wpf.ps1
+++ eng/copy-wpf.ps1
@@ -27,34 +27,6 @@ function Print-Usage()
 $RepoRoot = Resolve-Path (Join-Path $PSScriptRoot "..")
 $Config = if ($release) { "Release" } else { "Debug" }
 
-if ($help -or [string]::IsNullOrEmpty($destination)) 
-{
-    Print-Usage
-}
-elseif($testhost)
-{
-    Write-Host "Copying binaries to test host installation"
-    $location = Resolve-Path (Join-Path $destination "shared\Microsoft.WindowsDesktop.App\*")
-    if(![System.IO.File]::Exists($location))
-    {
-        Write-Host "Location unavailable: " $destination -ForegroundColor Red
-        return
-    }
-    CopyBinariesToLocation $location
-}
-else
-{
-    $runtimeIdentifer = if ($arch -eq "x86") { "win-x86" } else { "win-x64" }
-    $location = [System.IO.Path]::Combine($destination, "bin\Debug\netcoreapp3.0", $runtimeIdentifer, "publish")
-    if(![System.IO.File]::Exists($location))
-    {
-        Write-Host "Location unavailable: " $location -ForegroundColor Red
-        return
-    }
-    Write-Host "Copying binaries to app published directory"
-    CopyBinariesToLocation $location  
-}
-
 function CopyBinariesToLocation($location)
 {
     $locallyBuiltBinaryLocationBase = Join-Path $RepoRoot "artifacts\bin"
@@ -63,6 +35,7 @@ function CopyBinariesToLocation($location)
     CopyThemeBinariesToLocation $location  $locallyBuiltBinaryLocationBase
     CopyUIAutomationBinariesToLocation $location  $locallyBuiltBinaryLocationBase
 }
+
 function CopyNativeBinariesToLocation($location, $localBinLocation)
 {
     $NativeBinaries = "D3DCompiler", "PenImc", "PresentationNative", "wpfgfx"
@@ -81,7 +54,7 @@ function CopyNativeBinariesToLocation($location, $localBinLocation)
 function CopyManagedBinariesToLocation($location, $localBinLocation)
 {
     # x86 managed binaries don't have a distinct folder
-    $ManagedBinaries = "DirectWriteForwarder", "PresentationCore", "PresentationCore-CommonResources", "PresentationUI", "PresentationFramework-SystemCore", "PresentationFramework-SystemData", "PresentationFramework-SystemXml", "PresenationFramework-SystemXmlLinq", "ReachFramework", "System.Printing", "System.Windows.Controls.Ribbon", "System.Windows.Input.Manipulations", "WindowsBase", "WindowsFormsIntegration"
+    $ManagedBinaries = "DirectWriteForwarder", "PresentationCore", "PresentationCore-CommonResources", "PresentationUI", "PresentationFramework-SystemCore", "PresentationFramework-SystemData", "PresentationFramework-SystemXml", "PresenationFramework-SystemXmlLinq", "ReachFramework", "System.Printing", "System.Windows.Controls.Ribbon", "System.Windows.Input.Manipulations", "WindowsBase", "WindowsFormsIntegration", "System.Xaml"
     CopyNetCoreApp3Binaries $location $localBinLocation $ManagedBinaries
 }
 function CopyThemeBinariesToLocation($location, $localBinLocation)
@@ -118,5 +91,33 @@ function CopyBinaryToLocation($binary, $binaryLocation, $destination)
     {
         Write-Host "Binary Unavailable: " $binary -ForegroundColor Yellow
     }
+}
+
+if ($help -or [string]::IsNullOrEmpty($destination)) 
+{
+    Print-Usage
+}
+elseif($testhost)
+{
+    Write-Host "Copying binaries to test host installation"
+    $location = Resolve-Path (Join-Path $destination "shared\Microsoft.WindowsDesktop.App\*")
+    if(![System.IO.Directory]::Exists($location))
+    {
+        Write-Host "Location unavailable: " $location -ForegroundColor Red
+        return
+    }
+    CopyBinariesToLocation $location
+}
+else
+{
+    $runtimeIdentifer = if ($arch -eq "x86") { "win-x86" } else { "win-x64" }
+    $location = [System.IO.Path]::Combine($destination, "bin\Debug\netcoreapp3.0", $runtimeIdentifer, "publish")
+    if(![System.IO.Directory]::Exists($location))
+    {
+        Write-Host "Location unavailable: " $location -ForegroundColor Red
+        return
+    }
+    Write-Host "Copying binaries to app published directory"
+    CopyBinariesToLocation $location  
 }
 

de9098a7c4619c66f8c2a0cdb5d25bc74462b2b3
Steven Kirbach <steven.kirbach@gmail.com>
2019-04-23 15:24:54 -0700

    adding initial helix support
    note: this definitely won't work, it's just some basic initial infrastructure for us to build on once we have a better idea of what to do
    
    use dummy HelixWorkItem initially
    
    updating pipeline.yml
    
    fix Release only condition

---
+++ eng/helix/configure-helix-machine.ps1
@@ -0,0 +1,18 @@
+# This file prepares the helix machine for our tests runs
+
+$dotnetLocation = Join-Path (Split-Path -Parent $script:MyInvocation.MyCommand.Path) "dotnet"
+
+Write-Output "Adding dotnet location to path: $dotnetLocation"
+Write-Output "Path before: $env:path"
+# Prepend the path to ensure that dotnet.exe is called from there.
+$env:path="$dotnetLocation;$env:path"
+
+Write-Output "Path after: $env:path"
+# Don't look outside the TestHost to resolve best match SDKs.
+$env:DOTNET_MULTILEVEL_LOOKUP = 0
+
+# Disable first run since we do not need all ASP.NET packages restored.
+$env:DOTNET_SKIP_FIRST_TIME_EXPERIENCE=1
+
+# Ensure that the dotnet installed at our dotnetLocation is used
+$env:DOTNET_ROOT=$dotnetLocation

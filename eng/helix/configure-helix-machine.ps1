29721fd3574cd0f2163ef1785860d9199a825dab
Steven Kirbach <steven.kirbach@gmail.com>
2019-05-02 15:43:07 -0700

    move to using Microsoft.DotNet.Wpf.DncEng.Test package name

--- eng/helix/configure-helix-machine.ps1
+++ eng/helix/configure-helix-machine.ps1
@@ -3,16 +3,15 @@
 $dotnetLocation = Join-Path (Split-Path -Parent $script:MyInvocation.MyCommand.Path) "dotnet"
 
 Write-Output "Adding dotnet location to path: $dotnetLocation"
-Write-Output "Path before: $env:path"
-# Prepend the path to ensure that dotnet.exe is called from there.
-$env:path="$dotnetLocation;$env:path"
 
-Write-Output "Path after: $env:path"
+# Prepend the path to ensure that dotnet.exe is called from there.
+[System.Environment]::SetEnvironmentVariable("PATH", "$dotnetLocation;$env:path", [System.EnvironmentVariableTarget]::User)
+
 # Don't look outside the TestHost to resolve best match SDKs.
-$env:DOTNET_MULTILEVEL_LOOKUP = 0
+[System.Environment]::SetEnvironmentVariable("DOTNET_MULTILEVEL_LOOKUP", "0", [System.EnvironmentVariableTarget]::User)
 
 # Disable first run since we do not need all ASP.NET packages restored.
-$env:DOTNET_SKIP_FIRST_TIME_EXPERIENCE=1
+[System.Environment]::SetEnvironmentVariable("DOTNET_SKIP_FIRST_TIME_EXPERIENCE", "1", [System.EnvironmentVariableTarget]::User)
 
 # Ensure that the dotnet installed at our dotnetLocation is used
-$env:DOTNET_ROOT=$dotnetLocation
+[System.Environment]::SetEnvironmentVariable("DOTNET_SKIP_FIRST_TIME_EXPERIENCE", "$dotnetLocation", [System.EnvironmentVariableTarget]::User)

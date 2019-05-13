d60ab2f538eb1a1c06905a0b52744441ecd23a23
神樹桜乃 <kodama@sakuno.moe>
2019-05-13 17:02:34 +0800

    Simplify code

--- src/Microsoft.DotNet.Wpf/src/PresentationFramework/System/Windows/SystemResourceKey.cs
+++ src/Microsoft.DotNet.Wpf/src/PresentationFramework/System/Windows/SystemResourceKey.cs
@@ -364,10 +364,6 @@ namespace System.Windows
             short memberId = 0;
             string srkField = null;
             bool isKey = false;
-            bool found = true;
-
-            // Initialization needed to keep compiler happy!
-            SystemResourceKeyID srkId = SystemResourceKeyID.InternalSystemColorsStart;
 
             if (memberName.EndsWith("Key", false, TypeConverterHelper.InvariantEnglishUS))
             {
@@ -388,12 +384,8 @@ namespace System.Windows
             }
 
             if (targetType.Assembly == XamlTypeMapper.AssemblyPF &&
-                targetType.FullName == "System.Windows.SystemParameters")
-            {
-                found = Enum.TryParse(srkField, out srkId);
-            }
-
-            if (found)
+                targetType.FullName == "System.Windows.SystemParameters" &&
+                Enum.TryParse(srkField, out SystemResourceKeyID srkId))
             {
                 bool isExtended = ((short)srkId > SystemResourceKeyIDExtendedStart &&
                                    (short)srkId < SystemResourceKeyIDExtendedEnd);

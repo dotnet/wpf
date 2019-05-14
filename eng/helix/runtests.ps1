12254118a60f100c7af6f6a2d6179dd91ff7213f
Steven Kirbach <steven.kirbach@gmail.com>
2019-05-13 23:06:48 -0700

    validate the environment variable is set

--- eng/helix/runtests.ps1
+++ eng/helix/runtests.ps1
@@ -6,6 +6,8 @@ Param(
 # Configure the machine before running tests
 . "$PSScriptRoot\configure-helix-machine.ps1"
 
+Get-Variable "DOTNET_ROOT(X86)"
+
 # Run the tests
 $testLocation = Join-Path (Split-Path -Parent $script:MyInvocation.MyCommand.Path) "Test"
 if (Test-Path "$testLocation\rundrts.cmd")

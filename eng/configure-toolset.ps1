4a49ef4d18922c1855ee6a3f90cf7a968f041248
Ryland <41491307+ryalanms@users.noreply.github.com>
2019-05-22 12:13:56 -0700

    WPF transport package consumed by WPF-int containing configure-toolset.ps1 is causing WPF-int to fail.  Replace with MSBuild task instead.

--- eng/configure-toolset.ps1
+++ eng/configure-toolset.ps1
@@ -2,37 +2,6 @@
 # This file should be kept in sync across https://www.github.com/dotnet/wpf and dotnet-wpf-int repos. 
 #
 
-## Install pre-commit WPF Git hook to protect WPF's generated files
-function InstallCustomWPFGitHooks {
-  $WPFPreCommitGitHookSource = Join-Path $RepoRoot "eng\wpfarcadesdk\tools\pre-commit.githook"
-  $WPFPreCommitGitHookDest = Join-Path $RepoRoot ".git\hooks\pre-commit"
-
-  if (-not (Test-Path $WPFPreCommitGitHookSource)) {
-    Write-Host "WPF PreCommit GitHook file is missing: $WPFPreCommitGitHookSource"
-    ExitWithExitCode 1
-  }
-
-  Write-Host "Detecting WPF Git hooks..."
-
-  if (-not (Test-Path $WPFPreCommitGitHookDest)) {
-     Write-Host "Installing WPF Git pre-commit hook..."
-     try {
-        Copy-Item -Path $WPFPreCommitGitHookSource -Destination $WPFPreCommitGitHookDest 
-     }
-     catch {
-      Write-Host "Error: WPF Git pre-commit hook installation failed!"
-      Write-Host $_
-      Write-Host $_.Exception
-      Write-Host $_.ScriptStackTrace
-      ExitWithExitCode 1
-    }
-  }
-  else
-  {
-     Write-Host "Detected existing WPF Git pre-commit hook."
-  }
-}
-
 # This repo uses C++/CLI /clr:pure (or /clr:netcore) switches during compilation, which are 
 # deprecated. Ensure that this warning is always suppressed during build.
 if (($properties -eq $null) -or (-not ($properties -icontains '/nowarn:D9035'))) {
@@ -57,9 +26,4 @@ if ($ci) {
 # Always generate binary logs
 $binaryLog = $true
 $DoNotAbortNativeToolsInstallationOnFailure = $true
-
-# Always install custom WPF Git hooks when not in the CI build
-if (-not ($ci)) {
-    InstallCustomWPFGitHooks 
-}
 

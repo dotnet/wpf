affa6615f50f2ddd4e84360fb420471d60c8e4c0
Steven Kirbach <steven.kirbach@gmail.com>
2019-05-30 11:12:37 -0700

    running tests with build -test
    this creates the payload as part of msbuild

---
+++ eng/WpfArcadeSdk/tools/runtests.ps1
@@ -0,0 +1,29 @@
+[CmdLetBinding()]
+Param(
+    [string]$command
+)
+
+# Configure the machine before running tests
+if (Test-Path "$PSScriptRoot\configure-helix-machine.ps1")
+{
+    . "$PSScriptRoot\configure-helix-machine.ps1"
+}
+
+# Run the tests
+$testLocation = Join-Path (Split-Path -Parent $script:MyInvocation.MyCommand.Path) "Test"
+if (Test-Path "$testLocation\rundrts.cmd")
+{
+    Invoke-Expression "$testLocation\rundrts.cmd $command"
+}
+
+# We can use $env:HELIX_PYTHONPATH $env:HELIX_SCRIPT_ROOT\upload_result.py to upload any QV specific logs and/or screenshots that we are interested in.
+# For example: $env:HELIX_PYTHONPATH $env:HELIX_SCRIPT_ROOT%\upload_result.py -result screenshot.jpg -result_name screenshot.jpg
+# Then, links to these artifacts can then be included in the xUnit logs.
+
+# Need to copy the xUnit log to a known location that helix can understand
+if (Test-Path "$env:AppData\QualityVault\Run\Report\testResults.xml")
+{
+    $resultLocation = $PSScriptRoot
+    Write-Output "Copying test results to $resultLocation"
+    Copy-Item "$env:AppData\QualityVault\Run\Report\testResults.xml" $resultLocation
+}

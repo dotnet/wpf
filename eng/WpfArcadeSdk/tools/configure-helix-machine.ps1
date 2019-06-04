d8b1f9037b1a351847af97c747a34803be34e3a6
Steven Kirbach <steven.kirbach@gmail.com>
2019-06-04 10:59:28 -0700

    ensure ResolveFrameworkReferences actually works

--- eng/WpfArcadeSdk/tools/configure-helix-machine.ps1
+++ eng/WpfArcadeSdk/tools/configure-helix-machine.ps1
@@ -1,9 +1,4 @@
 # This file prepares the helix machine for our tests runs
-$dotnetLocation = Join-Path (Split-Path -Parent $script:MyInvocation.MyCommand.Path) "dotnet"
-
-# Set DOTNET_ROOT variables so the host can find it
-Set-Item -Path "env:DOTNET_ROOT(x86)" -Value $dotnetLocation
-Set-Item -Path "env:DOTNET_ROOT" -Value $dotnetLocation
 
 # Temporary workaround until https://github.com/dotnet/wpf/issues/816 is addressed.
 # The test infrastructure is built against an older version of Microsoft.WindowsDesktop.App.

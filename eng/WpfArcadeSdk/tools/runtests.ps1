8379bca415b63dc1e150e44013c304c5f1ee8446
Steven Kirbach <steven.kirbach@gmail.com>
2019-06-04 10:59:28 -0700

    ensure ResolveFrameworkReferences actually works

--- eng/WpfArcadeSdk/tools/runtests.ps1
+++ eng/WpfArcadeSdk/tools/runtests.ps1
@@ -1,13 +1,31 @@
 [CmdLetBinding()]
 Param(
-    [string]$command
+    [string]$command,
+    [switch]$ci
 )
 
-# Configure the machine before running tests
-if (Test-Path "$PSScriptRoot\configure-helix-machine.ps1")
+if ($ci)
 {
+    # When running in ci, the dotnet install is located at %HELIX_CORRELATION_PAYLOAD% along with all our test content.
+    $dotnetLocation = Join-Path (Split-Path -Parent $script:MyInvocation.MyCommand.Path) "dotnet"
+    # Only either the x86 or x64 versions of dotnet are installed on the helix machine, and they both go to the same location
+    $x86dotnetLocation = $dotnetLocation
+
+    # Run any extra machine setup required for Helix
     . "$PSScriptRoot\configure-helix-machine.ps1"
 }
+else
+{
+    # When running local, we run out of $(RepoRoot)artifacts\test\$(Configuration)\$(Platform)
+    # The dotnet install is located at $(RepoRoot).dotnet
+    $dotnetLocation =  Join-Path (Split-Path -Parent $script:MyInvocation.MyCommand.Path) "..\..\..\..\.dotnet" -Resolve
+    # The x86 location installed by Arcade is in the x86 directory
+    $x86dotnetLocation = "$dotnetLocation\x86"
+}
+
+# Set DOTNET_ROOT variables so the host can find it
+Set-Item -Path "env:DOTNET_ROOT(x86)" -Value $x86dotnetLocation
+Set-Item -Path "env:DOTNET_ROOT" -Value $dotnetLocation
 
 # Run the tests
 $testLocation = Join-Path (Split-Path -Parent $script:MyInvocation.MyCommand.Path) "Test"

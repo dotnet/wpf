f7100d65bf02e6381db6a169d6ddcdebb36ff2a2
Dilip Ojha <dilip.ojha@microsoft.com>
2019-06-24 02:01:13 -0700

    added internalencoding class

---
+++ src/Microsoft.DotNet.Wpf/src/Shared/MS/Internal/Text/InternalEncoding.cs
@@ -0,0 +1,33 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Text;
+
+namespace MS.Internal.Text
+{
+    // We use encodings that are not provided by default in core.
+    // This class makes sure that we register extra providers that are required before use.
+    internal static class InternalEncoding
+    {
+
+        static InternalEncoding()
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+        }
+
+        internal static Encoding GetEncoding(int codepage)
+        {
+            return Encoding.GetEncoding(codepage);
+        }
+
+        internal static Encoding Default
+        {
+            get
+            {
+                return Encoding.Default;
+            }
+        }
+
+    };
+}

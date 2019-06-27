317a27c46e2f800a24974d970bf71a75b98060ab
Steven Kirbach <steven.kirbach@gmail.com>
2019-06-27 13:06:32 -0700

    fixing up tests

--- src/Microsoft.DotNet.Wpf/test/DigitalDocuments/DRT/Metro/InterleavedPackage/DrtInterleavedPackage.cs
+++ src/Microsoft.DotNet.Wpf/test/DigitalDocuments/DRT/Metro/InterleavedPackage/DrtInterleavedPackage.cs
@@ -130,44 +130,44 @@ public class DrtInterleavedPackage
             Console.WriteLine("\nCreateInterleavedPackage");
         }
 
-        Package package = OpenPackage("interleaved.zip", FileMode.Create, FileAccess.ReadWrite, FileShare.None);
-
-        // Add 2 parts, and fill them in an interleaved fashion.
-
-        PackagePart poemPart = package.CreatePart(new Uri(_poemUri, UriKind.Relative), "text/plain", CompressionOption.Normal);
-        Stream partStream1 = poemPart.GetStream(FileMode.Create);
-        StreamWriter partWriter1 = new StreamWriter(partStream1);
-       
-        PackagePart specPart = package.CreatePart(new Uri(_specUri, UriKind.Relative), "text/plain");
-        Stream partStream2 = specPart.GetStream(FileMode.Create);
-        StreamWriter partWriter2 = new StreamWriter(partStream2);
-
-        partWriter1.Write(_poemPieces[0]);
-        partWriter1.Flush();
-        partWriter2.Write(_specPieces[0]);
-        partWriter2.Flush();
-        partWriter2.Write(_specPieces[1]);
-        partWriter2.Flush();
-        partWriter2.Write(_specPieces[2]);
-        partWriter1.Write(_poemPieces[1]);
-        partWriter1.Flush();
-        partWriter2.Flush(); // A no-op.
-        partWriter1.Flush(); // A no-op.
-        partWriter1.Write(_poemPieces[2]);
-        partWriter2.Close();
-        partWriter1.Close();
-
-        // Add relationships.
-        for (int i = 0; i < _packageRelationships.Count; ++i)
+        using (var fileStream = new FileStream("interleaved.zip", FileMode.Create, FileAccess.ReadWrite, FileShare.None))
+        using (Package package = Package.Open(fileStream, FileMode.Create, FileAccess.ReadWrite))
         {
-            package.CreateRelationship(_targetUri, TargetMode.Internal, _packageRelationships[i]);
-        }
-        for (int i = 0; i < _specRelationships.Count; ++i)
-        {
-            specPart.CreateRelationship(_targetUri, TargetMode.Internal, _specRelationships[i]);
-        }
+            // Add 2 parts, and fill them in an interleaved fashion.
 
-        package.Close();
+            PackagePart poemPart = package.CreatePart(new Uri(_poemUri, UriKind.Relative), "text/plain", CompressionOption.Normal);
+            Stream partStream1 = poemPart.GetStream(FileMode.Create);
+            StreamWriter partWriter1 = new StreamWriter(partStream1);
+
+            PackagePart specPart = package.CreatePart(new Uri(_specUri, UriKind.Relative), "text/plain");
+            Stream partStream2 = specPart.GetStream(FileMode.Create);
+            StreamWriter partWriter2 = new StreamWriter(partStream2);
+
+            partWriter1.Write(_poemPieces[0]);
+            partWriter1.Flush();
+            partWriter2.Write(_specPieces[0]);
+            partWriter2.Flush();
+            partWriter2.Write(_specPieces[1]);
+            partWriter2.Flush();
+            partWriter2.Write(_specPieces[2]);
+            partWriter1.Write(_poemPieces[1]);
+            partWriter1.Flush();
+            partWriter2.Flush(); // A no-op.
+            partWriter1.Flush(); // A no-op.
+            partWriter1.Write(_poemPieces[2]);
+            partWriter2.Close();
+            partWriter1.Close();
+
+            // Add relationships.
+            for (int i = 0; i < _packageRelationships.Count; ++i)
+            {
+                package.CreateRelationship(_targetUri, TargetMode.Internal, _packageRelationships[i]);
+            }
+            for (int i = 0; i < _specRelationships.Count; ++i)
+            {
+                specPart.CreateRelationship(_targetUri, TargetMode.Internal, _specRelationships[i]);
+            }
+        }
         
         if (!CheckContentOfParts())
             return 1;
@@ -189,66 +189,67 @@ public class DrtInterleavedPackage
         // Make a copy of the unedited package.
 
         // Open the package for edit.
-        Package package = Package.Open("interleaved.zip", FileMode.Open, FileAccess.ReadWrite);
-
-        // Correct the spelling of "Acording" using the initial extra space.
-        PackagePart specPart = package.GetPart(new Uri(_specUri, UriKind.Relative));
-        Stream specStream = specPart.GetStream(FileMode.Open, FileAccess.ReadWrite);
-        specStream.Write(StringToByteArray("According"), 0, "According".Length);
-        specStream.Flush();
-
-        // Update _specPieces to reflect the edit.
-        _specPieces[0] = "According" + _specPieces[0].Substring("According".Length);
-        
-        // Read across contiguous pieces.
-        CheckContent(specStream, 10, _specPieces[0].Substring(10) + _specPieces[1].Substring(0, 17));
-        specStream.Close();
-
-        // Add to the poem, after using GetParts to retrieve it.
-        PackagePart poemPart = null;
-        foreach (PackagePart p in package.GetParts())
+        using (Package package = Package.Open("interleaved.zip", FileMode.Open, FileAccess.ReadWrite))
         {
-            if (string.Compare(p.Uri.OriginalString, _poemUri, StringComparison.Ordinal) == 0)
+            // Correct the spelling of "Acording" using the initial extra space.
+            PackagePart specPart = package.GetPart(new Uri(_specUri, UriKind.Relative));
+            using (Stream specStream = specPart.GetStream(FileMode.Open, FileAccess.ReadWrite))
             {
-                poemPart = p;
-                break;
+                specStream.Write(StringToByteArray("According"), 0, "According".Length);
+                specStream.Flush();
+
+                // Update _specPieces to reflect the edit.
+                _specPieces[0] = "According" + _specPieces[0].Substring("According".Length);
+
+                // Read across contiguous pieces.
+                CheckContent(specStream, 10, _specPieces[0].Substring(10) + _specPieces[1].Substring(0, 17));
             }
+
+            // Add to the poem, after using GetParts to retrieve it.
+            PackagePart poemPart = null;
+            foreach (PackagePart p in package.GetParts())
+            {
+                if (string.Compare(p.Uri.OriginalString, _poemUri, StringComparison.Ordinal) == 0)
+                {
+                    poemPart = p;
+                    break;
+                }
+            }
+            if (poemPart == null)
+            {
+                Console.Error.WriteLine("Unable to retrieve part \"/{0}\" from the package.", _poemUri);
+                return 1; // error
+            }
+
+            Stream poemStream = poemPart.GetStream(FileMode.Open, FileAccess.ReadWrite);
+            long originalTextSize = poemStream.Length;
+            poemStream.Seek(0, SeekOrigin.End);
+            string poemSequel = "    And this is thy dominion.\n";
+
+            poemStream.Write(StringToByteArray(poemSequel), 0, poemSequel.Length);
+            poemStream.Flush();
+
+            // Update _poemPieces to reflect the append.
+            _poemPieces.Add(poemSequel);
+
+            // Add a package relationship.
+            _packageRelationships.Add("Equivocation");
+            package.CreateRelationship(_targetUri, TargetMode.Internal,
+                _packageRelationships[_packageRelationships.Count - 1]);
+
+            // Remove a part relationship.
+            string typeToRemove = _specRelationships[0];
+            _specRelationships.RemoveAt(0);
+            string id = null;
+            foreach (PackageRelationship rel in specPart.GetRelationshipsByType(typeToRemove))
+            {
+                Assert(id == null);
+                id = rel.Id;
+            }
+            specPart.DeleteRelationship(id);
         }
-        if (poemPart == null)
-        {
-            Console.Error.WriteLine("Unable to retrieve part \"/{0}\" from the package.", _poemUri);
-            return 1; // error
-        }
-           
-        Stream poemStream = poemPart.GetStream(FileMode.Open, FileAccess.ReadWrite);
-        long originalTextSize = poemStream.Length;
-        poemStream.Seek(0, SeekOrigin.End);
-        string poemSequel = "    And this is thy dominion.\n";
 
-        poemStream.Write(StringToByteArray(poemSequel), 0, poemSequel.Length);
-        poemStream.Flush();
-
-        // Update _poemPieces to reflect the append.
-        _poemPieces.Add(poemSequel);
-
-        // Add a package relationship.
-        _packageRelationships.Add("Equivocation");
-        package.CreateRelationship(_targetUri, TargetMode.Internal,
-            _packageRelationships[_packageRelationships.Count-1]);
-
-        // Remove a part relationship.
-        string typeToRemove = _specRelationships[0];
-        _specRelationships.RemoveAt(0);
-        string id = null;
-        foreach (PackageRelationship rel in specPart.GetRelationshipsByType(typeToRemove))
-        {
-            Assert(id == null);
-            id = rel.Id;
-        }
-        specPart.DeleteRelationship(id);
-
-        package.Close();
-
+       
         if (!CheckContentOfParts())
             return 1;
 
@@ -256,14 +257,6 @@ public class DrtInterleavedPackage
             return 1;
 
         return 0;
-    }
-
-    private Package OpenPackage(string file, FileMode packageMode, FileAccess packageAccess, System.IO.FileShare packageShare)
-    {
-        using (var fileStream = new FileStream(file, packageMode, packageAccess, packageShare))
-        {
-            return Package.Open(fileStream, packageMode, packageAccess);
-        }
     }
 
     private bool CheckContent(Stream stream, long startPosition, string expectedString)
@@ -299,18 +292,18 @@ public class DrtInterleavedPackage
     private bool CheckContentOfParts()
     {
         bool success = true;
+        using (var fileStream = new FileStream("interleaved.zip", FileMode.Open, FileAccess.ReadWrite, FileShare.Read))
+        using (Package package = Package.Open(fileStream, FileMode.Open, FileAccess.ReadWrite))
+        {
+            // Check the current content of part _poemUri.
+            PackagePart poemPart = package.GetPart(new Uri(_poemUri, UriKind.Relative));
+            success = CheckPartContent(poemPart, _poemPieces);
 
-        Package package = Package.Open("interleaved.zip", FileMode.Open, FileAccess.Read, FileShare.Read);
+            // Check the current content of part _specUri.
+            PackagePart specPart = package.GetPart(new Uri(_specUri, UriKind.Relative));
+            success &= CheckPartContent(specPart, _specPieces);
+        }
 
-        // Check the current content of part _poemUri.
-        PackagePart poemPart = package.GetPart(new Uri(_poemUri, UriKind.Relative));
-        success = CheckPartContent(poemPart, _poemPieces);
-
-        // Check the current content of part _specUri.
-        PackagePart specPart = package.GetPart(new Uri(_specUri, UriKind.Relative));
-        success &= CheckPartContent(specPart, _specPieces);
-
-        package.Close();
         return success;
     }
 
@@ -318,53 +311,54 @@ public class DrtInterleavedPackage
     {
         bool success = true;
 
-        Package package = Package.Open("interleaved.zip", FileMode.Open, FileAccess.Read);
-        PackagePart poemPart = package.GetPart(new Uri(_poemUri, UriKind.Relative));
-        PackagePart specPart = package.GetPart(new Uri(_specUri, UriKind.Relative));
-        
-        int numRelationships = 0;
-        foreach (string name in _packageRelationships)
+        using (Package package = Package.Open("interleaved.zip", FileMode.Open, FileAccess.Read))
         {
-            ++numRelationships;
-            if (Count(package.GetRelationshipsByType(name)) != 1)
+            PackagePart poemPart = package.GetPart(new Uri(_poemUri, UriKind.Relative));
+            PackagePart specPart = package.GetPart(new Uri(_specUri, UriKind.Relative));
+
+            int numRelationships = 0;
+            foreach (string name in _packageRelationships)
             {
-                Console.Error.WriteLine("Found {0} instead of exactly 1 package relationship with name {1}",
-                    Count(package.GetRelationshipsByType(name)), name);
-                success = false;
+                ++numRelationships;
+                if (Count(package.GetRelationshipsByType(name)) != 1)
+                {
+                    Console.Error.WriteLine("Found {0} instead of exactly 1 package relationship with name {1}",
+                        Count(package.GetRelationshipsByType(name)), name);
+                    success = false;
+                }
+                else if (_verbose)
+                {
+                    Console.WriteLine("Package contains relationship {0}.", name);
+                }
             }
-            else if (_verbose)
+            if (numRelationships != Count(package.GetRelationships()))
             {
-                Console.WriteLine("Package contains relationship {0}.", name);
+                Console.Error.WriteLine("Found {0} instead of {1} package relationships.",
+                    Count(package.GetRelationships()), numRelationships);
+            }
+
+            numRelationships = 0;
+            foreach (string name in _specRelationships)
+            {
+                ++numRelationships;
+                if (Count(specPart.GetRelationshipsByType(name)) != 1)
+                {
+                    Console.Error.WriteLine("Found {0} instead of exactly 1 specPart relationship with name {1}",
+                        Count(specPart.GetRelationshipsByType(name)), name);
+                    success = false;
+                }
+                else if (_verbose)
+                {
+                    Console.WriteLine("SpecPart contains relationship {0}.", name);
+                }
+            }
+            if (numRelationships != Count(specPart.GetRelationships()))
+            {
+                Console.Error.WriteLine("Found {0} instead of {1} specPart relationships.",
+                    Count(specPart.GetRelationships()), numRelationships);
             }
         }
-        if (numRelationships != Count(package.GetRelationships()))
-        {
-            Console.Error.WriteLine("Found {0} instead of {1} package relationships.",
-                Count(package.GetRelationships()), numRelationships);
-        }
-                    
-        numRelationships = 0;
-        foreach (string name in _specRelationships)
-        {
-            ++numRelationships;
-            if (Count(specPart.GetRelationshipsByType(name)) != 1)
-            {
-                Console.Error.WriteLine("Found {0} instead of exactly 1 specPart relationship with name {1}",
-                    Count(specPart.GetRelationshipsByType(name)), name);
-                success = false;
-            }
-            else if (_verbose)
-            {
-                Console.WriteLine("SpecPart contains relationship {0}.", name);
-            }
-        }
-        if (numRelationships != Count(specPart.GetRelationships()))
-        {
-            Console.Error.WriteLine("Found {0} instead of {1} specPart relationships.",
-                Count(specPart.GetRelationships()), numRelationships);
-        }
-                    
-        package.Close();
+
         return success;
     }
 
@@ -372,13 +366,15 @@ public class DrtInterleavedPackage
     {
         bool success = true;
         long readOffset = 0;
-        Stream partStream = part.GetStream(FileMode.Open, FileAccess.Read);
-
-        for (int i = 0; i < expectedContentFragments.Count; ++i)
+        using (Stream partStream = part.GetStream(FileMode.Open, FileAccess.Read))
         {
-            success &= CheckContent(partStream, readOffset, expectedContentFragments[i]);
-            readOffset += expectedContentFragments[i].Length;
+            for (int i = 0; i < expectedContentFragments.Count; ++i)
+            {
+                success &= CheckContent(partStream, readOffset, expectedContentFragments[i]);
+                readOffset += expectedContentFragments[i].Length;
+            }
         }
+
         return success;
     }
 

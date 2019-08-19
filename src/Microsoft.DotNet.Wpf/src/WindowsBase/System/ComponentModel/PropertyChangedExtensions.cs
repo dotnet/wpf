7f6cfc0ddc35aabddf198772309cbcae85b8b5e9
Jogge <15981279+Jogge@users.noreply.github.com>
2019-08-19 13:04:22 +0200

    Create RaisePropertyChanged extension for INotifyPropertyChanged

---
+++ src/Microsoft.DotNet.Wpf/src/WindowsBase/System/ComponentModel/PropertyChangedExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace System.ComponentModel
+{
+    using System.Reflection;
+    using System.Runtime.CompilerServices;
+
+    public static class PropertyChangedExtensions
+    {
+        public static void RaisePropertyChanged(this INotifyPropertyChanged @this, [CallerMemberName] string propertyName = null)
+        {
+            var declaringType = @this.GetType().GetEvent(nameof(INotifyPropertyChanged.PropertyChanged)).DeclaringType;
+            var propertyChangedFieldInfo = declaringType.GetField(nameof(INotifyPropertyChanged.PropertyChanged), BindingFlags.Instance | BindingFlags.NonPublic);
+            var propertyChangedEventHandler = propertyChangedFieldInfo.GetValue(@this) as PropertyChangedEventHandler;
+            propertyChangedEventHandler?.Invoke(@this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}

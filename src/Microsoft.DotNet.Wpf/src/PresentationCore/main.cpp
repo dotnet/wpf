31571d9a819c00e9cd8f4c5296b2b50e8d1bdd3c
Rob LaDuca <roladuca@microsoft.com>
2019-08-20 20:42:33 -0700

    Further work on merging PCore back - Some file re-arrangement - Adding over-arching merge project - Updating PCoreCSharp to use PCoreCPP
      Author:    Rob LaDuca <roladuca@microsoft.com>

--- src/Microsoft.DotNet.Wpf/src/PresentationCore/main.cpp
+++ src/Microsoft.DotNet.Wpf/src/PresentationCore/main.cpp
@@ -2,7 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-#include "CPP/precomp.hxx"
 #include <shlwapi.h>
 #include "Utils.hxx" // from shared\inc
 #include "dwriteloader.h" // from shared\inc

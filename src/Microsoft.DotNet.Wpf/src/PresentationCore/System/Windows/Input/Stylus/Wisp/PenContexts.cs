d40a4669964f3c3ea64365e73f98ae5682f8137c
Rob LaDuca <roladuca@microsoft.com>
2019-10-29 12:25:43 -0700

    Updating usage of transform functions in WISP stylus stack to specifically use the per-mon aware functions.

--- src/Microsoft.DotNet.Wpf/src/PresentationCore/System/Windows/Input/Stylus/Wisp/PenContexts.cs
+++ src/Microsoft.DotNet.Wpf/src/PresentationCore/System/Windows/Input/Stylus/Wisp/PenContexts.cs
@@ -512,7 +512,7 @@ namespace System.Windows.Input
                 ptTablet = ptTablet * stylusDevice.TabletDevice.TabletDeviceImpl.TabletToScreen;
                 ptTablet.X = (int)Math.Round(ptTablet.X); // Make sure we snap to whole window pixels.
                 ptTablet.Y = (int)Math.Round(ptTablet.Y);
-                ptTablet = _stylusLogic.MeasureUnitsFromDeviceUnits(ptTablet); // change to measured units now.
+                ptTablet = _stylusLogic.MeasureUnitsFromDeviceUnits(stylusDevice.CriticalActiveSource, ptTablet); // change to measured units now.
 
                 pic = HittestPlugInCollection(ptTablet); // Use cached rectangles for UIElements.
             }
@@ -555,6 +555,8 @@ namespace System.Windows.Input
 
             return null;
         }
+
+        internal HwndSource InputSource { get { return _inputSource?.Value; } }
 
         /////////////////////////////////////////////////////////////////////
 

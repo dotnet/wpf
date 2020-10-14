9c723ef3dd5d682383fc90fa3a5f6fbf990b7158
Ryland <41491307+ryalanms@users.noreply.github.com>
2020-10-13 17:08:56 -0700

    Revert "Merge pull request #3637 from dotnet/revertPath"
    This reverts commit 85c890f056855ba11b1e8fd9e922f3c1c78e9213, reversing
    changes made to 5800b00ee9595fd62006b75e62faadb1784f58a0.

--- src/Microsoft.DotNet.Wpf/src/PresentationBuildTasks/MS/Internal/MarkupCompiler/MarkupCompiler.cs
+++ src/Microsoft.DotNet.Wpf/src/PresentationBuildTasks/MS/Internal/MarkupCompiler/MarkupCompiler.cs
@@ -1575,19 +1575,11 @@ namespace MS.Internal
         {
             get
             {
-                string parentFolderPrefix = string.Empty;
-                if (TargetPath.StartsWith(SourceFileInfo.SourcePath, StringComparison.OrdinalIgnoreCase))
-                {
-                    string relPath = TargetPath.Substring(SourceFileInfo.SourcePath.Length);
-                    relPath += SourceFileInfo.RelativeSourceFilePath;
-                    string[] dirs = relPath.Split(new Char[] { Path.DirectorySeparatorChar });
-                    for (int i = 1; i < dirs.Length; i++)
-                    {
-                        parentFolderPrefix += PARENTFOLDER;
-                    }
-                }
-
-                return parentFolderPrefix;
+#if NETFX 
+                return PathInternal.GetRelativePath(TargetPath, SourceFileInfo.SourcePath, StringComparison.OrdinalIgnoreCase) + Path.DirectorySeparatorChar;
+#else
+                return Path.GetRelativePath(TargetPath, SourceFileInfo.SourcePath) + Path.DirectorySeparatorChar;
+#endif
             }
         }
 

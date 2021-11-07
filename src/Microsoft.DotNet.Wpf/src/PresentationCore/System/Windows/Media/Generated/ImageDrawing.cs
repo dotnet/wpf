11fcead7f58e5f8ed2eeaf237fe28462ef2742d0
lindexi <lindexi_gd@163.com>
2021-11-07 15:59:58 +0800

    Remove unuse static field in ImageDrawing.cs

--- src/Microsoft.DotNet.Wpf/src/PresentationCore/System/Windows/Media/Generated/ImageDrawing.cs
+++ src/Microsoft.DotNet.Wpf/src/PresentationCore/System/Windows/Media/Generated/ImageDrawing.cs
@@ -327,8 +327,6 @@ namespace System.Windows.Media
 
         internal System.Windows.Media.Composition.DUCE.MultiChannelResource _duceResource = new System.Windows.Media.Composition.DUCE.MultiChannelResource();
 
-        internal static Rect s_Rect = Rect.Empty;
-
         #endregion Internal Fields
 
 

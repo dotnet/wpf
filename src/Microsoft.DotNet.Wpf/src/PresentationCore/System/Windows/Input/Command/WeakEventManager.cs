5b0373e72e7d3f2c3a8a149689c54944b6e4f5fc
Ibrahim Ahmed Badr <56772211+Ibrahimogod@users.noreply.github.com>
2022-02-19 09:55:41 +0200

    Add Missing namespaces to WeakEventManager.cs

--- src/Microsoft.DotNet.Wpf/src/PresentationCore/System/Windows/Input/Command/WeakEventManager.cs
+++ src/Microsoft.DotNet.Wpf/src/PresentationCore/System/Windows/Input/Command/WeakEventManager.cs
@@ -1,10 +1,12 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Reflection;
+using System.Runtime.CompilerServices;
 
-namespace PresentationCore.System.Windows.Input.Command
+namespace System.Windows.Input
 {
     internal class WeakEventManager
     {

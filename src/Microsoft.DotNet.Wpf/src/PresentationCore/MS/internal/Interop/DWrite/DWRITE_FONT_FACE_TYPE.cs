b59b4542aca1d4087f45a95d565c2f97164c8d0a
ThomasGoulet73 <thomas.goulet-73@outlook.com>
2022-02-23 00:19:23 -0500

    Add license + cleanup

--- src/Microsoft.DotNet.Wpf/src/PresentationCore/MS/internal/Interop/DWrite/DWRITE_FONT_FACE_TYPE.cs
+++ src/Microsoft.DotNet.Wpf/src/PresentationCore/MS/internal/Interop/DWrite/DWRITE_FONT_FACE_TYPE.cs
@@ -1,3 +1,6 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
 namespace MS.Internal.Interop.DWrite
 {
     internal enum DWRITE_FONT_FACE_TYPE
--- src/Microsoft.DotNet.Wpf/src/PresentationCore/MS/internal/Text/TextInterface/DWriteUtil.cs
+++ src/Microsoft.DotNet.Wpf/src/PresentationCore/MS/internal/Text/TextInterface/DWriteUtil.cs
@@ -1,4 +1,7 @@
-﻿namespace MS.Internal.Text.TextInterface
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace MS.Internal.Text.TextInterface
 {
     internal static class DWriteUtil
     {

28c3986360bc696316fda21d03e1c0d1828fcfb2
Dipesh Kumar <85861525+dipeshmsft@users.noreply.github.com>
2022-08-27 00:04:51 +0530

    Modified templates to use Implicit Usings

---
+++ packaging/Microsoft.Dotnet.Wpf.ProjectTemplates/content/WpfApplication-CSharp/net6.0/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿#if (!csharpFeature_ImplicitUsings)
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+#endif
+
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Company.WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ packaging/Microsoft.Dotnet.Wpf.ProjectTemplates/content/WpfClassLibrary-CSharp/net6.0/Class1.cs
@@ -0,0 +1,10 @@
+#if (!csharpFeature_ImplicitUsings)
+using System;
+#endif
+
+namespace Company.ClassLibrary1
+{
+    public class Class1
+    {
+    }
+}

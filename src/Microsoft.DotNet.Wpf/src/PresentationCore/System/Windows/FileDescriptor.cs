a8bb49c687fd19153d899a33f712788e11bd9f9b
miloush <miloush@users.noreply.github.com>
2023-04-20 13:25:51 +0100

    XML docs, file split

--- src/Microsoft.DotNet.Wpf/src/PresentationCore/System/Windows/FileDescriptor.cs
+++ src/Microsoft.DotNet.Wpf/src/PresentationCore/System/Windows/FileDescriptor.cs
@@ -3,174 +3,67 @@
 // See the LICENSE file in the project root for more information.
 
 using System.IO;
-using System.Collections.Generic;
-using System.Collections;
-
-using SR = MS.Internal.PresentationCore.SR;
 
 namespace System.Windows
 {
-    public class FileGroup : IReadOnlyList<KeyValuePair<FileDescriptor, Stream>>
-    {
-        private List<FileDescriptor> _fileDescriptors;
-        private List<Stream> _streams;
-        private IDataObjectWithIndex _dataObject;
-
-        public FileGroup()
-        {
-            _fileDescriptors = new List<FileDescriptor>();
-            _streams = new List<Stream>();
-        }
-        internal FileGroup(IDataObjectWithIndex dataObject, IEnumerable<FileDescriptor> fileDescriptors)
-        {
-            if (dataObject == null)
-            {
-                throw new ArgumentNullException(nameof(dataObject));
-            }
-
-            _dataObject = dataObject;
-            _fileDescriptors = new List<FileDescriptor>(fileDescriptors);
-        }
-
-        public int Count
-        {
-            get { return _fileDescriptors.Count; }
-        }
-        public bool IsReadOnly
-        {
-            get { return _streams == null; }
-        }
-
-        public IReadOnlyList<FileDescriptor> FileDescriptors
-        {
-            get { return _fileDescriptors.AsReadOnly(); }
-        }
-
-        public void Add(string filename, byte[] data)
-        {
-            if (filename == null)
-            {
-                throw new ArgumentNullException(nameof(filename));
-            }
-
-            if (data == null)
-            {
-                throw new ArgumentNullException(nameof(data));
-            }
-
-            Add(new FileDescriptor(filename), new MemoryStream(data));
-        }
-        public void Add(string filename, Stream stream)
-        {
-            if (filename == null)
-            {
-                throw new ArgumentNullException(nameof(filename));
-            }
-
-            Add(new FileDescriptor(filename), stream);
-        }
-        public void Add(FileDescriptor descriptor, Stream stream)
-        {
-            if (descriptor == null)
-            {
-                throw new ArgumentNullException(nameof(descriptor));
-            }
-
-            // stream can be null for directories
-
-            if (IsReadOnly)
-            {
-                throw new InvalidOperationException(SR.FileGroup_ReadOnly);
-            }
-
-            _fileDescriptors.Add(descriptor);
-            _streams.Add(stream);
-        }
-
-        public Stream GetFileContents(FileDescriptor descriptor)
-        {
-            int index = _fileDescriptors.IndexOf(descriptor);
-            if (index < 0)
-            {
-                throw new KeyNotFoundException(SR.FileGroup_DescriptorNotFound);
-            }
-
-            return GetFileContents(index);
-        }
-        public Stream GetFileContents(int index)
-        {
-            if (index < 0 || index >= _fileDescriptors.Count)
-            {
-                throw new ArgumentOutOfRangeException(nameof(index));
-            }
-
-            if (_dataObject != null)
-            {
-                if (_fileDescriptors[index].IsDirectory)
-                {
-                    return null;
-                }
-
-                object data = null;
-                try
-                {
-                    data = _dataObject.GetData(DataFormats.FileContents, false, index);
-                }
-                catch (NotImplementedException) { } // e.g. folder contents
-
-                if (data is Stream stream)
-                {
-                    return stream;
-                }
-                else if (data is byte[] buffer)
-                {
-                    return new MemoryStream(buffer);
-                }
-                else
-                {
-                    return null; // if we throw here, the enumerator will throw unrecoverably
-                }
-            }
-            else
-            {
-                return _streams[index];
-            }
-        }
-
-        public IEnumerator<KeyValuePair<FileDescriptor, Stream>> GetEnumerator()
-        {
-            for (int i = 0; i < Count; i++)
-            {
-                yield return KeyValuePair.Create(_fileDescriptors[i], GetFileContents(i));
-            }
-        }
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            return GetEnumerator();
-        }
-
-        KeyValuePair<FileDescriptor, Stream> IReadOnlyList<KeyValuePair<FileDescriptor, Stream>>.this[int index]
-        {
-            get { return KeyValuePair.Create(_fileDescriptors[index], GetFileContents(index)); }
-        }
-    }
-
+    /// <summary>
+    /// Describes the properties of a file for data transfer. 
+    /// </summary>
     public class FileDescriptor
     {
+        /// <summary>
+        /// Gets or sets the file type identifier.
+        /// </summary>
         public Guid? Clsid { get; set; }
+
+        /// <summary>
+        /// Gets or sets the screen coordinates of the file object.
+        /// </summary>
         public Int32Rect? Icon { get; set; }
+
+        /// <summary>
+        /// Gets or sets the file attribute flags.
+        /// </summary>
         public FileAttributes? FileAttributes { get; set; }
+
+        /// <summary>
+        /// Gets or sets the time of file creation.
+        /// </summary>
         public DateTime? CreationTime { get; set; }
+
+        /// <summary>
+        /// Gets or sets the time that the file was last accessed.
+        /// </summary>
         public DateTime? LastAccessTime { get; set; }
+
+        /// <summary>
+        /// Gets or sets the time of the last write operation.
+        /// </summary>
         public DateTime? LastWriteTime { get; set; }
+
+        /// <summary>
+        /// Gets or sets the file sizes, in bytes.
+        /// </summary>
         public long? FileSize { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name of the file.
+        /// </summary>
         public string FileName { get; }
 
+        /// <summary>
+        /// Gets whether the file represents a directory.
+        /// </summary>
         public bool IsDirectory
         {
             get { return (FileAttributes.GetValueOrDefault() & IO.FileAttributes.Directory) != 0; }
         }
 
+        /// <summary>
+        /// Initializes a new instance of <see cref="FileDescriptor"/> with given file name.
+        /// </summary>
+        /// <param name="filename">A relative file name.</param>
+        /// <exception cref="ArgumentNullException"><paramref name="filename"/> is <see langword="null"/>.</exception>
         public FileDescriptor(string filename)
         {
             if (filename == null)
@@ -181,6 +74,11 @@ namespace System.Windows
             FileName = filename;
         }
 
+        /// <summary>
+        /// Initializes a new instance of <see cref="FileDescriptor"/> representing a physical file or directory.
+        /// </summary>
+        /// <param name="path">A path to the file or directory.</param>
+        /// <returns>an instance of <see cref="FileDescriptor"/> representing given file or directory.</returns>
         public static FileDescriptor FromFile(string path)
         {
             FileSystemInfo info;
@@ -197,6 +95,7 @@ namespace System.Windows
 
             return FromFile(info);
         }
+
         private static FileDescriptor FromFile(FileSystemInfo info)
         {
             if (info == null)

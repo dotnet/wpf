ac7600d9612088882fc2297356b81bb50a96481f
Bellamy <hughbellars@gmail.com>
2023-05-02 20:04:03 +0100

    Remove dead code from ReflectionHelper

--- src/Microsoft.DotNet.Wpf/src/Shared/System/Windows/Markup/ReflectionHelper.cs
+++ src/Microsoft.DotNet.Wpf/src/Shared/System/Windows/Markup/ReflectionHelper.cs
@@ -79,60 +79,45 @@ namespace System.Xaml
 
 #region Type
         /// <summary>
-        /// Parse and get the type of the passed in string
+        /// Parse and get the type of the passed-in string.
         /// </summary>
         internal static Type GetQualifiedType(string typeName)
         {
-            // ISSUE: we only parse the assembly name and type name
-            // all other Type.GetType() type fragments (version, culture info, pub key token etc) are ignored!!!
-            string[] nameFrags = typeName.Split(new Char[] { ',' }, 2);
-            Type type = null;
+            // We only parse the assembly name and type name.
+            // All other Type.GetType() type fragments (version, culture info, public
+            // key token etc) are ignored.
+            string[] nameFrags = typeName.Split(new char[] { ',' }, 2);
             if (nameFrags.Length == 1)
             {
-                // treat it as an absolute name
-                type = Type.GetType(nameFrags[0]);
+                // Treat this as an absolute name.
+                return Type.GetType(nameFrags[0]);
             }
-            else
+
+            Assembly a = null;
+            try
             {
-                if (nameFrags.Length != 2)
-                    throw new InvalidOperationException(SR.Format(SR.QualifiedNameHasWrongFormat, typeName));
-
-                Assembly a = null;
-                try
-                {
-                    a = LoadAssembly(nameFrags[1].TrimStart(), null);
-                }
-                // ifdef magic to save compiler update.
-                // the fix below is for an FxCop rule about non-CLR exceptions.
-                // however this rule has now been removed.
-                catch (Exception e)   // Load throws generic Exceptions, so this can't be made more specific.
-                {
-                    if (CriticalExceptions.IsCriticalException(e))
-                    {
-                        throw;
-                    }
-                    else
-                    {
-                        // If we can't load the assembly, just return null (fall-through).
-                        a = null;
-                    }
-                }
-
-                if (a != null)
-                {
-                    try
-                    {
-                        type = a.GetType(nameFrags[0]);
-                        // If we can't get the type, just return null (fall-through).
-                    }
-                    catch (ArgumentException)
-                    {
-                        a = null;
-                    }
-                }
+                a = LoadAssembly(nameFrags[1].TrimStart(), null);
+            }
+            catch (Exception e) when (!CriticalExceptions.IsCriticalException(e))
+            {
             }
 
-            return type;
+            // If we can't load the assembly, just return null.
+            if (a == null)
+            {
+                return null;
+            }
+
+            try
+            {
+                return a.GetType(nameFrags[0]);
+            }
+            catch (ArgumentException)
+            {
+            }
+
+            // If we can't get the type, just return null.
+            return null;
         }
 
         internal static bool IsNullableType(Type type)
@@ -196,10 +181,12 @@ namespace System.Xaml
             return type;
         }
 
+#if PBTCOMPILER
         internal static Type GetMscorlibType(Type type)
         {
             return GetFrameworkType(MscorlibReflectionAssemblyName, type);
         }
+#endif
 
         internal static Type GetSystemType(Type type)
         {
@@ -516,10 +503,12 @@ namespace System.Xaml
             return isFriend;
         }
 
+#if PBTCOMPILER
         internal static bool IsInternalAllowedOnType(Type type)
         {
             return ((LocalAssemblyName == type.Assembly.GetName().Name) || IsFriendAssembly(type.Assembly));
         }
+#endif
 
         // The local assembly that contains the baml.
         internal static string LocalAssemblyName

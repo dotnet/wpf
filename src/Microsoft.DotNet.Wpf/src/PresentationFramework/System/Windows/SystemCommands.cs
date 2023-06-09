58b1fe2d463aeeae6da60e0d7f7d7158935f3c42
Jan Kučera <10546952+miloush@users.noreply.github.com>
2023-06-09 10:41:24 +0100

    Allow right click in system menu (#7743)
    Co-authored-by: miloush <miloush@users.noreply.github.com>

--- src/Microsoft.DotNet.Wpf/src/PresentationFramework/System/Windows/SystemCommands.cs
+++ src/Microsoft.DotNet.Wpf/src/PresentationFramework/System/Windows/SystemCommands.cs
@@ -76,6 +76,7 @@ namespace System.Windows
         {
             const uint TPM_RETURNCMD = 0x0100;
             const uint TPM_LEFTBUTTON = 0x0;
+            const uint TPM_RIGHTBUTTON = 0x2;
 
             Verify.IsNotNull(window, "window");
             IntPtr hwnd = new WindowInteropHelper(window).Handle;
@@ -86,7 +87,7 @@ namespace System.Windows
 
             IntPtr hmenu = NativeMethods.GetSystemMenu(hwnd, false);
 
-            uint cmd = NativeMethods.TrackPopupMenuEx(hmenu, TPM_LEFTBUTTON | TPM_RETURNCMD, (int)physicalScreenLocation.X, (int)physicalScreenLocation.Y, hwnd, IntPtr.Zero);
+            uint cmd = NativeMethods.TrackPopupMenuEx(hmenu, TPM_LEFTBUTTON | TPM_RIGHTBUTTON | TPM_RETURNCMD, (int)physicalScreenLocation.X, (int)physicalScreenLocation.Y, hwnd, IntPtr.Zero);
             if (0 != cmd)
             {
                 NativeMethods.PostMessage(hwnd, WM.SYSCOMMAND, new IntPtr(cmd), IntPtr.Zero);

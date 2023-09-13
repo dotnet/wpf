2e394c6188dafe88f6bd00e1659ad97fd7400fef
Hugh Bellamy <hughbellars@gmail.com>
2023-09-13 10:20:48 +0100

    Add WindowsBase tests

---
+++ src/Microsoft.DotNet.Wpf/tests/UnitTests/WindowsBase.Tests/System/Windows/Threading/DispatcherHookEventArgsTests.cs
@@ -0,0 +1,27 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace System.Windows.Threading.Tests;
+
+public class DispatcherHookEventArgsTests
+{
+    [Fact]
+    public void Ctor_DispatcherOperation()
+    {
+        Dispatcher dispatcher = Dispatcher.CurrentDispatcher;
+        DispatcherOperation operation = dispatcher.BeginInvoke(() => {});
+
+        var args = new DispatcherHookEventArgs(operation);
+        Assert.Same(dispatcher, args.Dispatcher);
+        Assert.Same(operation, args.Operation);
+    }
+
+    [Fact]
+    public void Ctor_NullOperation()
+    {
+        var args = new DispatcherHookEventArgs(null);
+        Assert.Null(args.Dispatcher);
+        Assert.Null(args.Operation);
+    }
+}

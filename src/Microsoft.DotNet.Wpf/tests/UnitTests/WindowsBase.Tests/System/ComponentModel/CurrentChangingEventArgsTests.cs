eb8d8d2397e10c548534a8d0b256b563eda9ec1f
Hugh Bellamy <hughbellars@gmail.com>
2023-09-13 10:20:48 +0100

    Add WindowsBase tests

---
+++ src/Microsoft.DotNet.Wpf/tests/UnitTests/WindowsBase.Tests/System/ComponentModel/CurrentChangingEventArgsTests.cs
@@ -0,0 +1,63 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+namespace System.ComponentModel.Tests;
+
+public class CurrentChangingEventArgsTests
+{
+    [Fact]
+    public void Ctor_Default()
+    {
+        var args = new CurrentChangingEventArgs();
+        Assert.True(args.IsCancelable);
+        Assert.False(args.Cancel);
+    }
+    
+    [Theory]
+    [InlineData(true)]
+    [InlineData(false)]
+    public void Ctor_Bool(bool isCancelable)
+    {
+        var args = new CurrentChangingEventArgs(isCancelable);
+        Assert.Equal(isCancelable, args.IsCancelable);
+        Assert.False(args.Cancel);
+    }
+
+    [Theory]
+    [InlineData(true)]
+    [InlineData(false)]
+    public void Cancel_Set_GetReturnsExpected(bool value)
+    {
+        var args = new CurrentChangingEventArgs();
+
+        // Set.
+        args.Cancel = value;
+        Assert.Equal(value, args.Cancel);
+
+        // Set same.
+        args.Cancel = value;
+        Assert.Equal(value, args.Cancel);
+
+        // Set different.
+        args.Cancel = !value;
+        Assert.Equal(!value, args.Cancel);
+    }
+
+    [Fact]
+    public void Cancel_SetFalseNotCancellable_Success()
+    {
+        var args = new CurrentChangingEventArgs(false);
+
+        // Set.
+        args.Cancel = false;
+        Assert.False(args.Cancel);
+    }
+
+    [Fact]
+    public void Cancel_SetTrueNotCancellable_ThrowsInvalidOperationException()
+    {
+        var args = new CurrentChangingEventArgs(false);
+        Assert.Throws<InvalidOperationException>(() => args.Cancel = true);
+    }
+}

6ef43b55f84dfb33ccf375167df72bdfc5e16b33
Rishabh Chauhan <rchauhan@microsoft.com>
2023-10-09 17:02:20 +0530

    Replace BF for string input

---
+++ src/Microsoft.DotNet.Wpf/src/PresentationCore/System/Windows/BinaryFormatWriterScope.cs
@@ -0,0 +1,33 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Text;
+using System.IO;
+
+namespace System.Windows
+{
+    internal readonly ref struct BinaryFormatWriterScope
+    {
+        private readonly BinaryWriter _writer;
+
+        public BinaryFormatWriterScope(Stream stream)
+        {
+            _writer = new BinaryWriter(stream, Encoding.UTF8, leaveOpen: true);
+            //SerializationHeader.Default.Write(_writer);
+        }
+
+        public static implicit operator BinaryWriter(in BinaryFormatWriterScope scope) => scope._writer;
+
+        public void Write(string value)
+        {
+            _writer.Write(value);
+        }
+        public void Dispose()
+        {
+            //MessageEnd.Instance.Write(_writer);
+            _writer.Dispose();
+        }
+    }
+}
+

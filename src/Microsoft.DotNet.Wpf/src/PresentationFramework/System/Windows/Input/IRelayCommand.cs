143a8ec8edea3c4f30200eeb95eb8e1aa73e39b7
Anjali <anjalisheel@microsoft.com>
2023-12-22 11:11:41 +0530

    Added Styles for TextBox

---
+++ src/Microsoft.DotNet.Wpf/src/PresentationFramework/System/Windows/Input/IRelayCommand.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.Windows.Input
+{
+    /// <summary>
+    /// An interface expanding <see cref="ICommand"/> with the ability to raise
+    /// the <see cref="ICommand.CanExecuteChanged"/> event externally.
+    /// </summary>
+    public interface IRelayCommand : ICommand
+    {
+        /// <summary>
+        /// Notifies that the <see cref="ICommand.CanExecute"/> property has changed.
+        /// </summary>
+        void NotifyCanExecuteChanged();
+    }
+}

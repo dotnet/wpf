8e4fd3d5c7523a7565f71a5fa6e1bea207d81f3b
Ashish Kumar Singh <singhashish@microsoft.com>
2023-12-27 14:19:09 +0530

    Add ControlsDictionary and ThemesDictionary

---
+++ src/Microsoft.DotNet.Wpf/src/PresentationFramework/System/Windows/Markup/ThemesDictionary.cs
@@ -0,0 +1,57 @@
+// This Source Code Form is subject to the terms of the MIT License.
+// If a copy of the MIT was not distributed with this file, You can obtain one at https://opensource.org/licenses/MIT.
+// Copyright (C) Leszek Pomianowski and WPF UI Contributors.
+// All Rights Reserved.
+
+using System.Windows.Markup;
+using System.Windows.Controls;
+using System.Windows.Appearance;
+namespace System.Windows.Markup;
+
+/// <summary>
+/// Provides a dictionary implementation that contains <c>WPF UI</c> theme resources used by components and other elements of a WPF application.
+/// </summary>
+/// <example>
+/// <code lang="xml">
+/// &lt;Application
+///     xmlns:ui="http://schemas.lepo.co/wpfui/2022/xaml"&gt;
+///     &lt;Application.Resources&gt;
+///         &lt;ResourceDictionary&gt;
+///             &lt;ResourceDictionary.MergedDictionaries&gt;
+///                 &lt;ui:ThemesDictionary Theme = "Dark" /&gt;
+///             &lt;/ResourceDictionary.MergedDictionaries&gt;
+///         &lt;/ResourceDictionary&gt;
+///     &lt;/Application.Resources&gt;
+/// &lt;/Application&gt;
+/// </code>
+/// </example>
+[Localizability(LocalizationCategory.Ignore)]
+[Ambient]
+[UsableDuringInitialization(true)]
+public class ThemesDictionary : ResourceDictionary
+{
+    /// <summary>
+    /// Sets the default application theme.
+    /// </summary>
+    public ApplicationTheme Theme
+    {
+        set => SetSourceBasedOnSelectedTheme(value);
+    }
+
+    public ThemesDictionary()
+    {
+        SetSourceBasedOnSelectedTheme(ApplicationTheme.Light);
+    }
+
+    private void SetSourceBasedOnSelectedTheme(ApplicationTheme? selectedApplicationTheme)
+    {
+        var themeName = selectedApplicationTheme switch
+        {
+            ApplicationTheme.Dark => "Dark",
+            ApplicationTheme.HighContrast => "HighContrast",
+            _ => "Light"
+        };
+
+        Source = new Uri($"{ApplicationThemeManager.ThemesDictionaryPath}{themeName}.xaml", UriKind.Absolute);
+    }
+}

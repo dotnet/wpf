135e3b5f47e680313d6e062979143c71d7adce89
Anjali <anjalisheel@microsoft.com>
2024-01-03 10:25:24 +0530

    Adding win11 styles to RichTextBox and Separator (#8581)
    * Adding win11 styles to RichTextBox and Separator
    
    * Changed the IsReadOnly flag to "False"

--- src/Microsoft.DotNet.Wpf/src/PresentationFramework/System/Windows/Controls/RichTextBox.cs
+++ src/Microsoft.DotNet.Wpf/src/PresentationFramework/System/Windows/Controls/RichTextBox.cs
@@ -521,6 +521,25 @@ namespace System.Windows.Controls
             }
         }
 
+        /// <summary>
+        /// Property for <see cref="IsTextSelectionEnabledProperty"/>.
+        /// </summary>
+        public static readonly DependencyProperty IsTextSelectionEnabledProperty = DependencyProperty.Register(
+            nameof(IsTextSelectionEnabled),
+            typeof(bool),
+            typeof(RichTextBox),
+            new PropertyMetadata(false)
+        );
+
+        /// <summary>
+        /// TODO
+        /// </summary>
+        public bool IsTextSelectionEnabled
+        {
+            get => (bool)GetValue(IsTextSelectionEnabledProperty);
+            set => SetValue(IsTextSelectionEnabledProperty, value);
+        }
+
         // ...........................................................
         //
         // Content Accessing Properties

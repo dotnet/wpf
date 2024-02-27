3fefe774c746ee089b69892a948c810c7e326dd3
hmishra <hmishra@microsoft.com>
2024-02-27 14:45:03 +0530

    Code Cleanup and Removal of ControlsDictionary

--- src/Microsoft.DotNet.Wpf/src/Themes/PresentationFramework.Win11/Controls/ClientAreaBorder.cs
+++ src/Microsoft.DotNet.Wpf/src/Themes/PresentationFramework.Win11/Controls/ClientAreaBorder.cs
@@ -63,7 +63,7 @@ namespace PresentationFramework.Win11.Controls
 
         private static Thickness? _windowChromeNonClientFrameThickness;
 
-        public ApplicationTheme ApplicaitonTheme { get; set; } = ApplicationTheme.Unknown;
+        internal ApplicationTheme ApplicaitonTheme { get; set; } = ApplicationTheme.Unknown;
 
         /// <summary>
         /// Get the system <see cref="SM_CXPADDEDBORDER"/> value in WPF units.

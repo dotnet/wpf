0d22b5c8f94d79cded5fc7be9b25489f3f8fba96
h3xds1nz <gamingshard@protonmail.com>
2024-07-28 13:38:45 +0200

    convert CommandHelpers from params InputGesture[] to params ReadOnlySpan<InputGesture>

--- src/Microsoft.DotNet.Wpf/src/PresentationFramework/MS/Internal/Commands/CommandHelpers.cs
+++ src/Microsoft.DotNet.Wpf/src/PresentationFramework/MS/Internal/Commands/CommandHelpers.cs
@@ -20,7 +20,7 @@ namespace MS.Internal.Commands
 
         internal static void RegisterCommandHandler(Type controlType, RoutedCommand command, ExecutedRoutedEventHandler executedRoutedEventHandler)
         {
-            PrivateRegisterCommandHandler(controlType, command, executedRoutedEventHandler, null, null);
+            PrivateRegisterCommandHandler(controlType, command, executedRoutedEventHandler, null);
         }
 
         internal static void RegisterCommandHandler(Type controlType, RoutedCommand command, ExecutedRoutedEventHandler executedRoutedEventHandler,
@@ -44,7 +44,7 @@ namespace MS.Internal.Commands
         internal static void RegisterCommandHandler(Type controlType, RoutedCommand command, ExecutedRoutedEventHandler executedRoutedEventHandler,
                                                     CanExecuteRoutedEventHandler canExecuteRoutedEventHandler)
         {
-            PrivateRegisterCommandHandler(controlType, command, executedRoutedEventHandler, canExecuteRoutedEventHandler, null);
+            PrivateRegisterCommandHandler(controlType, command, executedRoutedEventHandler, canExecuteRoutedEventHandler);
         }
 
         internal static void RegisterCommandHandler(Type controlType, RoutedCommand command, ExecutedRoutedEventHandler executedRoutedEventHandler,
@@ -95,7 +95,7 @@ namespace MS.Internal.Commands
 
         // 'params' based method is private.  Call sites that use this bloat unwittingly due to implicit construction of the params array that goes into IL.
         private static void PrivateRegisterCommandHandler(Type controlType, RoutedCommand command, ExecutedRoutedEventHandler executedRoutedEventHandler,
-                                                          CanExecuteRoutedEventHandler canExecuteRoutedEventHandler, params InputGesture[] inputGestures)
+                                                          CanExecuteRoutedEventHandler canExecuteRoutedEventHandler, params ReadOnlySpan<InputGesture> inputGestures)
         {
             // Validate parameters
             Debug.Assert(controlType != null);
@@ -107,12 +107,9 @@ namespace MS.Internal.Commands
             CommandManager.RegisterClassCommandBinding(controlType, new CommandBinding(command, executedRoutedEventHandler, canExecuteRoutedEventHandler));
 
             // Create additional input binding for this command
-            if (inputGestures != null)
+            for (int i = 0; i < inputGestures.Length; i++)
             {
-                for (int i = 0; i < inputGestures.Length; i++)
-                {
-                    CommandManager.RegisterClassInputBinding(controlType, new InputBinding(command, inputGestures[i]));
-                }
+                CommandManager.RegisterClassInputBinding(controlType, new InputBinding(command, inputGestures[i]));
             }
         }
 

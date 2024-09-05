64f9b0ece6006234c38e52917f89b4f063a723b1
Dipesh Kumar <dipeshkumar@microsoft.com>
2024-09-05 17:49:17 +0530

    Added ThemeMode and ThemeModeConverter reference docs

--- src/Microsoft.DotNet.Wpf/src/PresentationFramework/System/Windows/ThemeMode.cs
+++ src/Microsoft.DotNet.Wpf/src/PresentationFramework/System/Windows/ThemeMode.cs
@@ -1,3 +1,7 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
 using System;
 using System.ComponentModel;
 
@@ -14,29 +18,113 @@ using System.Diagnostics.CodeAnalysis;
 
 namespace System.Windows
 {
+    /// <summary>
+    /// ThemeMode structure describes the Fluent theme mode to be applied
+    /// on Application or a Window.
+    /// </summary>
+    /// <remarks>
+    /// This is an experimental API and may be modified or removed in future releases.
+    /// Since this is an experimental API, rather than creating a new instance of ThemeMode,
+    /// use the static properties Light, Dark, System and None.
+    /// </remarks>
     [Experimental("WPF0001")]
     public readonly struct ThemeMode : IEquatable<ThemeMode>
     {
+        /// <summary>
+        /// Predefined theme mode : None.
+        /// This is the default value for Application.ThemeMode and Window.ThemeMode
+        /// and it means that Fluent theme will not be applied on the Application or Window.
+        /// </summary>
+        /// <remarks>
+        /// In case, when Application.ThemeNode is not set to None,
+        /// even if Window.ThemeMode is set to None, the Fluent theme will be applied on the Window.
+        /// </remarks>
         public static ThemeMode None => new ThemeMode();
+        
+        /// <summary>
+        /// Predefined theme mode : Light.
+        /// Whenever this mode is set on Application or Window, 
+        /// Fluent Light theme will be applied.
+        /// </summary>
         public static ThemeMode Light => new ThemeMode("Light");
+        
+        /// <summary>
+        /// Predefined theme mode : Dark.
+        /// Whenever this mode is set on Application or Window,
+        /// Fluent Dark theme will be applied.
+        /// </summary>
         public static ThemeMode Dark => new ThemeMode("Dark");
+        
+        /// <summary>
+        /// Perdefined theme mode : System.
+        /// Whenever this mode is set on Application or Window,
+        /// Fluent theme will be applied based on the system theme.
+        /// </summary>
         public static ThemeMode System => new ThemeMode("System");
 
-
+        /// <summary>
+        /// Gets the value of the ThemeMode.
+        /// </summary>
         public string Value => _value ?? "None";
 
+        /// <summary>
+        /// Creates a new ThemeMode object with the specified value.
+        /// </summary>
+        /// <param name="value">Name of theme mode</param>
         public ThemeMode(string value) => _value = value;
         
+        /// <summary>
+        /// Checks whether the ThemeMode object is equal to another ThemeMode object.
+        /// </summary>
+        /// <param name="other">ThemeMode object to compare with</param>
+        /// <returns>
+        /// Returns true when the ThemeMode objects are equal, and false otherwise.
+        /// </returns>
         public bool Equals(ThemeMode other) => string.Equals(Value, other.Value, StringComparison.Ordinal);
 
+        /// <summary>
+        /// Checks whether the object is equal to another ThemeMode object.
+        /// </summary>
+        /// <param name="obj">ThemeMode object to compare with.</param>
+        /// <returns>
+        /// Returns true when the ThemeMode objects are equal, and false otherwise.
+        /// </returns>
         public override bool Equals(object obj) => obj is ThemeMode other && Equals(other);
 
+        /// <summary>
+        /// Compute hash code for this object.
+        /// </summary>
+        /// <returns>A 32-bit signed integer hash code.</returns>
         public override int GetHashCode() => _value != null ? StringComparer.Ordinal.GetHashCode(_value) : 0;
 
+        /// <summary>
+        /// Checks whether two ThemeMode objects are equal.
+        /// </summary>
+        /// <param name="left">First ThemeMode object to compare</param>
+        /// <param name="right">Second ThemeMode object to compare</param>
+        /// <returns>
+        /// Returns true when both the ThemeMode objects are equal, 
+        /// and false otherwise.
+        /// </returns>
         public static bool operator ==(ThemeMode left, ThemeMode right) => left.Equals(right);
 
+        /// <summary>
+        /// Checks whether two ThemeMode objects are not equal.
+        /// </summary>
+        /// <param name="left">First ThemeMode object to compare</param>
+        /// <param name="right">Second ThemeMode object to compare</param>
+        /// <returns>
+        /// Returns true when the ThemeMode objects are not equal, 
+        /// and false otherwise.
+        /// </returns>
         public static bool operator !=(ThemeMode left, ThemeMode right) => !left.Equals(right);
 
+        /// <summary>
+        /// Creates a string representation of the ThemeMode object.
+        /// </summary>
+        /// <returns>
+        /// A string representation of this object.
+        /// </returns>
         public override string ToString() => Value;
 
         private readonly string _value;
--- src/Microsoft.DotNet.Wpf/src/PresentationFramework/System/Windows/ThemeModeConverter.cs
+++ src/Microsoft.DotNet.Wpf/src/PresentationFramework/System/Windows/ThemeModeConverter.cs
@@ -1,3 +1,7 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
 using System;
 using System.ComponentModel;
 
@@ -15,25 +19,55 @@ using System.Diagnostics.CodeAnalysis;
 
 namespace System.Windows
 {
+    /// <summary>
+    /// ThemeModeConverter - Converter class for converting instances 
+    /// of other types to and from ThemeMode instances.
+    /// </summary>
     [Experimental("WPF0001")]
-
     public class ThemeModeConverter: TypeConverter
     {
 
         #region Public Methods
 
+        /// <summary>
+        /// CanConvertFrom - Returns whether or not this class can convert from a given type.
+        /// </summary>
+        /// <param name="typeDescriptorContext">The ITypeDescriptorContext for this call.</param>
+        /// <param name="sourceType">The Type being queried for support.</param>
+        /// <returns>
+        /// bool - True if thie converter can convert from the provided type, false if not.
+        /// </returns>
         public override bool CanConvertFrom(ITypeDescriptorContext typeDescriptorContext, Type sourceType)
         {
            return Type.GetTypeCode(sourceType) == TypeCode.String;
         }
 
+        /// <summary>
+        /// CanConvertTo - Returns whether or not this class can convert to a given type.
+        /// </summary>
+        /// <param name="typeDescriptorContext"> The ITypeDescriptorContext for this call. </param>
+        /// <param name="destinationType"> The Type being queried for support. </param>
+        /// <returns>
+        /// bool - True if this converter can convert to the provided type, false if not.
+        /// </returns>
         public override bool CanConvertTo(ITypeDescriptorContext typeDescriptorContext, Type destinationType) 
         {
             // We can convert to an InstanceDescriptor or to a string.
             return destinationType == typeof(InstanceDescriptor) || destinationType == typeof(string);
         }
 
-
+        /// <summary>
+        /// ConvertFrom - Attempts to convert to a ThemeMode from the given object
+        /// </summary>
+        /// <exception cref="ArgumentNullException">
+        /// An ArgumentNullException is thrown if the example object is null.
+        /// </exception>
+        /// <param name="typeDescriptorContext">The ITypeDescriptorContext for this call.</param>
+        /// <param name="cultureInfo">The CultureInfo which is respected when converting.</param>
+        /// <param name="source">The object to convert to a ThemeMode.</param>
+        /// <returns>
+        /// ThemeMode instance which was constructed.
+        /// </returns>
         public override object ConvertFrom(ITypeDescriptorContext typeDescriptorContext, 
                                            CultureInfo cultureInfo, 
                                            object source)
@@ -46,6 +80,21 @@ namespace System.Windows
             throw GetConvertFromException(source);
         }
 
+        /// <summary>
+        /// ConvertTo - Attempts to convert a ThemeMode to the given type
+        /// </summary>
+        /// <exception cref="ArgumentNullException">
+        /// An ArgumentNullException is thrown if the example object is null.
+        /// </exception>
+        /// <exception cref="ArgumentException">
+        /// An ArgumentException is thrown if the object is not null and is not a ThemeMode,
+        /// or if the destinationType isn't one of the valid destination types.
+        /// </exception>
+        /// <param name="typeDescriptorContext">The ITypeDescriptorContext for this call.</param>
+        /// <param name="cultureInfo">The CultureInfo which is respected when converting.</param>
+        /// <param name="value">The ThemeMode to convert.</param>
+        /// <param name="destinationType">The type to which to convert the ThemeMode instance.</param>
+        /// <returns></returns>
         public override object ConvertTo(ITypeDescriptorContext typeDescriptorContext, 
                                          CultureInfo cultureInfo,
                                          object value,
@@ -67,7 +116,7 @@ namespace System.Windows
             }
             throw GetConvertToException(value, destinationType);
         }
-        #endregion 
 
+        #endregion 
     }
 }

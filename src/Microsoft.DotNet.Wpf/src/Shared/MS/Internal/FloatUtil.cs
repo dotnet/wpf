9eb636119393ae0ebdf1ef67396f107e31eb0885
h3xds1nz <gamingshard@protonmail.com>
2024-09-14 18:31:37 +0200

    Convert static float variables into compile-time constants

--- src/Microsoft.DotNet.Wpf/src/Shared/MS/Internal/FloatUtil.cs
+++ src/Microsoft.DotNet.Wpf/src/Shared/MS/Internal/FloatUtil.cs
@@ -12,10 +12,6 @@
 //              provides "fuzzy" comparison functionality for floats and
 //              float-based classes and structs in our code.
 //
-//
-//
-//
-//
 //---------------------------------------------------------------------------
 
 using System;
@@ -24,20 +20,23 @@ namespace MS.Internal
 {
     internal static class FloatUtil
     {
-        internal static float FLT_EPSILON  =   1.192092896e-07F;
-        internal static float FLT_MAX_PRECISION = 0xffffff;
-        internal static float INVERSE_FLT_MAX_PRECISION = 1.0F / FLT_MAX_PRECISION;
+        internal const float FLT_EPSILON = 1.192092896E-07f;
+        internal const float FLT_MAX_PRECISION = 0x00FFFFFF;
+        internal const float INVERSE_FLT_MAX_PRECISION = 1.0f / FLT_MAX_PRECISION;
 
         /// <summary>
         /// AreClose
         /// </summary>
         public static bool AreClose(float a, float b)
         {
-            if(a == b) return true;
+            if (a == b)
+                return true;
+
             // This computes (|a-b| / (|a| + |b| + 10.0f)) < FLT_EPSILON
             float eps = ((float)Math.Abs(a) + (float)Math.Abs(b) + 10.0f) * FLT_EPSILON;
             float delta = a - b;
-            return(-eps < delta) && (eps > delta);
+
+            return (-eps < delta) && (eps > delta);
         }
 
         /// <summary>
@@ -45,7 +44,7 @@ namespace MS.Internal
         /// </summary>
         public static bool IsOne(float a)
         {
-            return (float)Math.Abs(a-1.0f) < 10.0f * FLT_EPSILON;
+            return (float)Math.Abs(a - 1.0f) < 10.0f * FLT_EPSILON;
         }
 
         /// <summary>

ba0c5616d5588f86c784a32c4f07a83c891d412d
h3xds1nz <gamingshard@protonmail.com>
2024-10-06 13:31:35 +0200

    Simplify Equals override

--- src/Microsoft.DotNet.Wpf/src/PresentationFramework/System/Windows/GridLength.cs
+++ src/Microsoft.DotNet.Wpf/src/PresentationFramework/System/Windows/GridLength.cs
@@ -150,15 +150,9 @@ namespace System.Windows
         /// <param name="oCompare">Reference to an object for comparison.</param>
         /// <returns><c>true</c>if this GridLength instance has the same value 
         /// and unit type as oCompare.</returns>
-        override public bool Equals(object oCompare)
+        public override bool Equals(object oCompare)
         {
-            if(oCompare is GridLength)
-            {
-                GridLength l = (GridLength)oCompare;
-                return (this == l);
-            }
-            else
-                return false;
+            return oCompare is GridLength gridLength && Equals(gridLength);
         }
 
         /// <summary>

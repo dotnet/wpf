0e81d737e130edaf98aef526b2bb1a0404c70002
h3xds1nz <gamingshard@protonmail.com>
2024-10-12 12:02:09 +0200

    Auto format, fix documentation, cleanup

--- src/Microsoft.DotNet.Wpf/src/PresentationCore/System/Windows/Input/InputProcessorProfiles.cs
+++ src/Microsoft.DotNet.Wpf/src/PresentationCore/System/Windows/Input/InputProcessorProfiles.cs
@@ -2,43 +2,25 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 //
-// 
-//
 // Description: Creates ITfInputProcessorProfiles instances.
-//
 //
 
 using System.Runtime.InteropServices;
+using System.Globalization;
+using System.Diagnostics;
 using System.Threading;
 using MS.Win32;
-using System.Globalization;
-using System.Collections;
 
 namespace System.Windows.Input
 {
-    //------------------------------------------------------
-    //
-    //  InputProcessorProfiles class
-    //
-    //------------------------------------------------------
-
     /// <summary>
-    /// The InputProcessorProfiles class is always associated with 
-    /// hwndInputLanguage class.
+    /// The <see cref="InputProcessorProfiles"/> class is always associated with hwndInputLanguage class.
     /// </summary>
     internal class InputProcessorProfiles
     {
-        //------------------------------------------------------
-        //
-        //  Constructors
-        //
-        //------------------------------------------------------
-
         /// <summary>
         /// InputProcessorProfiles Constructor;
         /// </summary>
-        /// Critical - as this sets the value for _ipp.
-        /// Safe - as this just initializes it to null.
         internal InputProcessorProfiles()
         {
             // _ipp is a ValueType, hence no need for new.
@@ -46,14 +28,6 @@ namespace System.Windows.Input
             _cookie = UnsafeNativeMethods.TF_INVALID_COOKIE;
         }
 
-        //------------------------------------------------------
-        //
-        //  Internal Methods
-        //
-        //------------------------------------------------------
- 
-        #region Internal Methods
- 
         /// <summary>
         /// Initialize an interface and notify sink.
         /// </summary>
@@ -81,18 +55,10 @@ namespace System.Windows.Input
         {
             Debug.Assert(_ipp != null, "Uninitialize called without initializing");
 
-            UnadviseNotifySink();            
+            UnadviseNotifySink();
             Marshal.ReleaseComObject(_ipp);
             _ipp = null;
         }
-
-        #endregion Internal Methods
-
-        //------------------------------------------------------
-        //
-        //  Internal Properties
-        //
-        //------------------------------------------------------
 
         /// <summary>
         /// Get the current input language of the current thread.
@@ -112,7 +78,7 @@ namespace System.Windows.Input
                         IntPtr[] hklList = null;
 
                         int count = (int)SafeNativeMethods.GetKeyboardLayoutList(0, null);
-                        if (count > 1) 
+                        if (count > 1)
                         {
                             hklList = new IntPtr[count];
 
@@ -123,7 +89,7 @@ namespace System.Windows.Input
                             {
                                 if (value == (short)hklList[i])
                                 {
-                                    SafeNativeMethods.ActivateKeyboardLayout(new HandleRef(this,hklList[i]), 0);
+                                    SafeNativeMethods.ActivateKeyboardLayout(new HandleRef(this, hklList[i]), 0);
                                     break;
                                 }
                             }
@@ -157,13 +123,6 @@ namespace System.Windows.Input
             }
         }
 
-
-        //------------------------------------------------------
-        //
-        //  Private Methods
-        //
-        //------------------------------------------------------
-        
         /// <summary>
         /// This advices the input language notify sink to
         /// ITfInputProcessorProfile.
@@ -194,16 +153,14 @@ namespace System.Windows.Input
             _cookie = UnsafeNativeMethods.TF_INVALID_COOKIE;
         }
 
-        //------------------------------------------------------
-        //
-        //  Private Fields
-        //
-        //------------------------------------------------------
-                
-        // The reference to ITfInputProcessorProfile.
+        /// <summary>
+        /// The reference to <see cref="UnsafeNativeMethods.ITfInputProcessorProfiles"/>.
+        /// </summary>
         private UnsafeNativeMethods.ITfInputProcessorProfiles _ipp;
 
-        // The cookie for the advised sink.
+        /// <summary>
+        /// The cookie for the advised sink.
+        /// </summary>
         private int _cookie;
     }
 }

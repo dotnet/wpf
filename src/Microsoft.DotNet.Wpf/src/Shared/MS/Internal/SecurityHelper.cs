e4266d9998e85fa8e8859662f8e5a54de1a6bb0c
h3xds1nz <gamingshard@protonmail.com>
2024-10-21 15:22:11 +0200

    Remove GetExceptionForHR from SecurityHelper

--- src/Microsoft.DotNet.Wpf/src/Shared/MS/Internal/SecurityHelper.cs
+++ src/Microsoft.DotNet.Wpf/src/Shared/MS/Internal/SecurityHelper.cs
@@ -57,14 +57,6 @@ using MS.Internal.AppModel;
 #if REACHFRAMEWORK
     using MS.Internal.Utility;
 #endif
-#if WINDOWS_BASE
-    // This existed originally to allow FontCache service to 
-    // see the WindowsBase variant of this class. We no longer have
-    // a FontCache service, but over time other parts of WPF might
-    // have started to depend on this, so we leave it as-is for 
-    // compat. 
-#endif
-
 
 internal static class SecurityHelper
     {
@@ -130,13 +122,6 @@ internal static class SecurityHelper
         internal static int SizeOf(Object o)
         {
             return Marshal.SizeOf(o);
-        }
-#endif
-
-#if WINDOWS_BASE || PRESENTATION_CORE || PRESENTATIONFRAMEWORK
-        internal static Exception GetExceptionForHR(int hr)
-        {
-            return Marshal.GetExceptionForHR(hr, new IntPtr(-1));
         }
 #endif
 

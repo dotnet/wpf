dotnet/wpf

commit 557ee98663ee01d0dcda6371e82a366974c3d311

Remove bunch of allocations caused by static arrays by favoring ReadOnlySpan<T> (#9230)
* remove bunch of allocations caused by static arrays, fix ineffective codegens

* first call references a property, lets unify that

* address PR feedback from miloush, shave off few instructions each call

* minor format fix, lets make the pointer type clear

* fix failing CI build because it still targets net472 (net9 local build works fine)

src/Microsoft.DotNet.Wpf/src/PresentationCore/System/Windows/Media/RectangleGeometry.cs
@@ -220,10 +220,10 @@ namespace System.Windows.Media
                     // We've checked that rect isn't empty above
                     Invariant.Assert(pointCount != 0);
 
-                    Point * pPoints = stackalloc Point[(int)pointCount];
+                    Point* pPoints = stackalloc Point[(int)pointCount];
                     RectangleGeometry.GetPointList(pPoints, pointCount, rect, radiusX, radiusY);
 
-                    fixed (byte *pTypes = RectangleGeometry.GetTypeList(rect, radiusX, radiusY))
+                    fixed (byte* pTypes = GetTypeList(rect, radiusX, radiusY)) //Merely retrieves the pointer to static PE data, no actual pinning occurs
                     {
                         boundingRect = Geometry.GetBoundsHelper(
                             pen,
@@ -259,10 +259,10 @@ namespace System.Windows.Media
             
             unsafe
             {
-                Point *pPoints = stackalloc Point[(int)pointCount];
+                Point* pPoints = stackalloc Point[(int)pointCount];
                 RectangleGeometry.GetPointList(pPoints, pointCount, rect, radiusX, radiusY);
 
-                fixed (byte* pTypes = GetTypeList(rect, radiusX, radiusY))
+                fixed (byte* pTypes = GetTypeList(rect, radiusX, radiusY)) //Merely retrieves the pointer to static PE data, no actual pinning occurs
                 {
                     return ContainsInternal(
                         pen,
@@ -510,7 +510,7 @@ namespace System.Windows.Media
             }
         }
 
-        private static byte[] GetTypeList(Rect rect, double radiusX, double radiusY)
+        private static ReadOnlySpan<byte> GetTypeList(Rect rect, double radiusX, double radiusY)
         {
             if (rect.IsEmpty)
             {
@@ -518,11 +518,11 @@ namespace System.Windows.Media
             }
             else if (IsRounded(radiusX, radiusY))
             {
-                return s_roundedPathTypes;
+                return RoundedPathTypes;
             }
             else
             {
-                return s_squaredPathTypes;
+                return SquaredPathTypes;
             }
         }
 
@@ -610,30 +610,28 @@ namespace System.Windows.Media
 
         static private byte smoothLine = (byte)MILCoreSegFlags.SegTypeLine | (byte)MILCoreSegFlags.SegSmoothJoin;
 
-        static private byte[] s_roundedPathTypes = {
-            (byte)MILCoreSegFlags.SegTypeBezier | 
+        private static ReadOnlySpan<byte> RoundedPathTypes => new byte[] {
+            (byte)MILCoreSegFlags.SegTypeBezier |
             (byte)MILCoreSegFlags.SegIsCurved   |
-            (byte)MILCoreSegFlags.SegSmoothJoin | 
+            (byte)MILCoreSegFlags.SegSmoothJoin |
             (byte)MILCoreSegFlags.SegClosed,
-            smoothLine, 
+            smoothLine,
             smoothBezier,
-            smoothLine, 
+            smoothLine,
             smoothBezier,
-            smoothLine, 
+            smoothLine,
             smoothBezier,
-            smoothLine 
+            smoothLine
         };
 
         // Squared
         private const UInt32 c_squaredSegmentCount = 4;
         private const UInt32 c_squaredPointCount = 5;
 
-        private static readonly byte[] s_squaredPathTypes = {
-            (byte)MILCoreSegFlags.SegTypeLine | (byte)MILCoreSegFlags.SegClosed,
-            (byte)MILCoreSegFlags.SegTypeLine,
-            (byte)MILCoreSegFlags.SegTypeLine,
-            (byte)MILCoreSegFlags.SegTypeLine
-        };
+        private static ReadOnlySpan<byte> SquaredPathTypes => [(byte)MILCoreSegFlags.SegTypeLine | (byte)MILCoreSegFlags.SegClosed,
+                                                               (byte)MILCoreSegFlags.SegTypeLine,
+                                                               (byte)MILCoreSegFlags.SegTypeLine,
+                                                               (byte)MILCoreSegFlags.SegTypeLine];
 
         #endregion
     }

src/Microsoft.DotNet.Wpf/src/Common/Graphics/wgx_exports.cs
@@ -344,7 +344,7 @@ namespace MS.Internal
 
         internal static readonly Guid GUID_ContainerFormatWmp   = new Guid(0x57a37caa, 0x367a, 0x4540, 0x91, 0x6b, 0xf1, 0x83, 0xc5, 0x09, 0x3a, 0x4b);
 
-        internal static readonly byte[] GUID_VendorMicrosoft = new byte[] {  0xca,  0x49, 0xe7, 0xf0, 0xef, 0xed, 0x89, 0x45, 0xa7, 0x3a, 0xee, 0xe, 0x62, 0x6a, 0x2a, 0x2b };
+        internal static ReadOnlySpan<byte> GUID_VendorMicrosoft => [0xca, 0x49, 0xe7, 0xf0, 0xef, 0xed, 0x89, 0x45, 0xa7, 0x3a, 0xee, 0xe, 0x62, 0x6a, 0x2a, 0x2b];
     }
     #endregion // Guids
 }

src/Microsoft.DotNet.Wpf/src/PresentationBuildTasks/Microsoft/Build/Tasks/Windows/GenerateTemporaryTargetAssembly.cs
@@ -820,7 +820,7 @@ namespace Microsoft.Build.Tasks.Windows
                     XmlNode previousNodeImportProps = null;
                     XmlNode previousNodeImportTargets = null;
 
-                    foreach (string sdk in sdks.Split(_semicolonChar).Select(i => i.Trim()))
+                    foreach (string sdk in sdks.Split(s_semicolonChar).Select(i => i.Trim()))
                     {
                         //  <Project Sdk="Microsoft.NET.Sdk">
                         //  <Project Sdk="My.Custom.Sdk/1.0.0">
@@ -952,7 +952,7 @@ namespace Microsoft.Build.Tasks.Windows
 
         private const string WPFTMP = "wpftmp";
 
-        private static readonly char[] _semicolonChar = new char[] { ';' };
+        private static readonly char[] s_semicolonChar = [';'];
 
         #endregion Private Fields
 

src/Microsoft.DotNet.Wpf/src/PresentationCore/MS/internal/Ink/InkSerializedFormat/AlgoModule.cs
@@ -457,7 +457,7 @@ namespace MS.Internal.Ink.InkSerializedFormat
         internal const byte DefaultBAACount = 8;
         internal const byte MaxBAACount = 10;
 
+        private static ReadOnlySpan<double> DefaultFirstSquareRoot => [1, 1, 1, 4, 9, 16, 36, 49];
 
-        private static readonly double[] DefaultFirstSquareRoot = { 1, 1, 1, 4, 9, 16, 36, 49};
     }
 }

src/Microsoft.DotNet.Wpf/src/PresentationCore/MS/internal/Ink/InkSerializedFormat/InkSerializer.cs
@@ -281,14 +281,13 @@ namespace MS.Internal.Ink.InkSerializedFormat
             return;
         }
 
-        private static readonly byte[] Base64HeaderBytes
-                                            = new byte[]{(byte)'b',
-                                                        (byte)'a',
-                                                        (byte)'s',
-                                                        (byte)'e',
-                                                        (byte)'6',
-                                                        (byte)'4',
-                                                        (byte)':'};
+        private static ReadOnlySpan<byte> Base64HeaderBytes => [(byte)'b',
+                                                                (byte)'a',
+                                                                (byte)'s',
+                                                                (byte)'e',
+                                                                (byte)'6',
+                                                                (byte)'4',
+                                                                (byte)':'];
 
 #if OLD_ISF
         /// <summary>
@@ -2113,15 +2112,14 @@ namespace MS.Internal.Ink.InkSerializedFormat
             long currentPosition = data.Position;
             try
             {
-                byte[] isfBase64PrefixBytes = Base64HeaderBytes;
-                if (data.Length < isfBase64PrefixBytes.Length)
+                if (data.Length < Base64HeaderBytes.Length)
                 {
                     return false;
                 }
 
-                for (int x = 0; x < isfBase64PrefixBytes.Length; x++)
+                for (int x = 0; x < Base64HeaderBytes.Length; x++)
                 {
-                    if ((byte)data.ReadByte() != isfBase64PrefixBytes[x])
+                    if ((byte)data.ReadByte() != Base64HeaderBytes[x])
                     {
                         return false;
                     }

src/Microsoft.DotNet.Wpf/src/PresentationCore/MS/internal/Shaping/Positioning.cs
@@ -224,12 +224,14 @@ namespace MS.Internal.Shaping
         const ushort XAdvanceDeviceFlag  = 0x0040;
         const ushort YAdvanceDeviceFlag  = 0x0080;
         
-        private static ushort[] BitCount = 
-                    new ushort[16] { 0, 2, 2, 4,  2, 4, 4, 6,  2, 4, 4, 6,  4, 6, 6, 8 };
+        private static ReadOnlySpan<ushort> BitCount => [0, 2, 2, 4,
+                                                         2, 4, 4, 6,
+                                                         2, 4, 4, 6,
+                                                         4, 6, 6, 8];
 
         public static ushort Size(ushort Format)
         {
-            return (ushort)(BitCount[Format&0x000F]+BitCount[(Format>>4)&0x000F]);
+            return (ushort)(BitCount[Format & 0x000F] + BitCount[(Format >> 4) & 0x000F]);
         }
          
         public void AdjustPos(  FontTable Table,

src/Microsoft.DotNet.Wpf/src/PresentationCore/System/Windows/Input/Cursor.cs
@@ -354,7 +354,7 @@ namespace System.Windows.Input
 
         private SafeHandle  _cursorHandle;
 
-        private static readonly int[] CursorTypes = {
+        private static ReadOnlySpan<int> CursorTypes => [
             0, // None
             NativeMethods.IDC_NO,
             NativeMethods.IDC_ARROW,
@@ -383,6 +383,6 @@ namespace System.Windows.Input
             NativeMethods.IDC_ARROW + 149, // ScrollSWCursor
             NativeMethods.IDC_ARROW + 150, // ScrollSECursor
             NativeMethods.IDC_ARROW + 151 // ArrowCDCursor
-       };
+       ];
     }
 }

src/Microsoft.DotNet.Wpf/src/PresentationCore/System/Windows/Media/EllipseGeometry.cs
@@ -179,10 +179,10 @@ namespace System.Windows.Media
             {
                 unsafe
                 {
-                    Point * pPoints = stackalloc Point[(int)c_pointCount];
+                    Point* pPoints = stackalloc Point[(int)c_pointCount];
                     EllipseGeometry.GetPointList(pPoints, c_pointCount, center, radiusX, radiusY);
 
-                    fixed (byte *pTypes = EllipseGeometry.s_roundedPathTypes)
+                    fixed (byte* pTypes = RoundedPathTypes) //Merely retrieves the pointer to static PE data, no actual pinning occurs
                     {
                         rect = Geometry.GetBoundsHelper(
                             pen, 
@@ -206,10 +206,10 @@ namespace System.Windows.Media
         {
             unsafe
             {
-                Point *pPoints = stackalloc Point[(int)GetPointCount()];
+                Point* pPoints = stackalloc Point[(int)GetPointCount()];
                 EllipseGeometry.GetPointList(pPoints, GetPointCount(), Center, RadiusX, RadiusY);
 
-                fixed (byte* pTypes = GetTypeList())
+                fixed (byte* pTypes = RoundedPathTypes) //Merely retrieves the pointer to static PE data, no actual pinning occurs
                 {
                     return ContainsInternal(
                         pen,
@@ -383,9 +383,8 @@ namespace System.Windows.Media
             points[8].Y = points[9].Y = points[10].Y = center.Y - radiusY;
         }
 
-        private byte[] GetTypeList() { return s_roundedPathTypes; }
-        private uint GetPointCount() { return c_pointCount; }
-        private uint GetSegmentCount() { return c_segmentCount; }
+        private static uint GetPointCount() { return c_pointCount; }
+        private static uint GetSegmentCount() { return c_segmentCount; }
         
         #region Static Data
         
@@ -396,18 +395,16 @@ namespace System.Windows.Media
         private const UInt32 c_pointCount = 13;
 
         private const byte c_smoothBezier = (byte)MILCoreSegFlags.SegTypeBezier  |
-                                              (byte)MILCoreSegFlags.SegIsCurved    |
-                                              (byte)MILCoreSegFlags.SegSmoothJoin;
+                                            (byte)MILCoreSegFlags.SegIsCurved    |
+                                            (byte)MILCoreSegFlags.SegSmoothJoin;
 
-        private static readonly byte[] s_roundedPathTypes = {
-            (byte)MILCoreSegFlags.SegTypeBezier | 
-            (byte)MILCoreSegFlags.SegIsCurved   |
-            (byte)MILCoreSegFlags.SegSmoothJoin | 
-            (byte)MILCoreSegFlags.SegClosed,
-            c_smoothBezier, 
-            c_smoothBezier, 
-            c_smoothBezier
-        };
+        private static ReadOnlySpan<byte> RoundedPathTypes => [(byte)MILCoreSegFlags.SegTypeBezier | 
+                                                               (byte)MILCoreSegFlags.SegIsCurved   |
+                                                               (byte)MILCoreSegFlags.SegSmoothJoin | 
+                                                               (byte)MILCoreSegFlags.SegClosed,
+                                                               c_smoothBezier, 
+                                                               c_smoothBezier, 
+                                                               c_smoothBezier];
 
         #endregion
     }

commit 39dedeb9615f1f1ac83ea06fe577816cf97d084c

Remove regression from #9230 that shouldn't pass review, turn static byte fields into constants

src/Microsoft.DotNet.Wpf/src/PresentationCore/System/Windows/Media/RectangleGeometry.cs
@@ -580,25 +580,23 @@ namespace System.Windows.Media
         static private UInt32 c_roundedSegmentCount = 8;
         static private UInt32 c_roundedPointCount = 17;
 
-        static private byte smoothBezier = (byte)MILCoreSegFlags.SegTypeBezier |
-                                            (byte)MILCoreSegFlags.SegIsCurved   |
-                                            (byte)MILCoreSegFlags.SegSmoothJoin;
+        private const byte SmoothBezier = (byte)MILCoreSegFlags.SegTypeBezier |
+                                          (byte)MILCoreSegFlags.SegIsCurved   |
+                                          (byte)MILCoreSegFlags.SegSmoothJoin;
 
-        static private byte smoothLine = (byte)MILCoreSegFlags.SegTypeLine | (byte)MILCoreSegFlags.SegSmoothJoin;
+        private const byte SmoothLine = (byte)MILCoreSegFlags.SegTypeLine | (byte)MILCoreSegFlags.SegSmoothJoin;
 
-        private static ReadOnlySpan<byte> RoundedPathTypes => new byte[] {
-            (byte)MILCoreSegFlags.SegTypeBezier |
-            (byte)MILCoreSegFlags.SegIsCurved   |
-            (byte)MILCoreSegFlags.SegSmoothJoin |
-            (byte)MILCoreSegFlags.SegClosed,
-            smoothLine,
-            smoothBezier,
-            smoothLine,
-            smoothBezier,
-            smoothLine,
-            smoothBezier,
-            smoothLine
-        };
+        private static ReadOnlySpan<byte> RoundedPathTypes => [(byte)MILCoreSegFlags.SegTypeBezier |
+                                                               (byte)MILCoreSegFlags.SegIsCurved   |
+                                                               (byte)MILCoreSegFlags.SegSmoothJoin |
+                                                               (byte)MILCoreSegFlags.SegClosed,
+                                                               SmoothLine,
+                                                               SmoothBezier,
+                                                               SmoothLine,
+                                                               SmoothBezier,
+                                                               SmoothLine,
+                                                               SmoothBezier,
+                                                               SmoothLine];
 
         // Squared
         private const UInt32 c_squaredSegmentCount = 4;
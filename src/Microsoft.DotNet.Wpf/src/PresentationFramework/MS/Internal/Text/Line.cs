2d334926fcc349a937a199c1639c7f11b99dbbe3
h3xds1nz <gamingshard@protonmail.com>
2024-10-24 22:45:01 +0200

    Remove a comment

--- src/Microsoft.DotNet.Wpf/src/PresentationFramework/MS/Internal/Text/Line.cs
+++ src/Microsoft.DotNet.Wpf/src/PresentationFramework/MS/Internal/Text/Line.cs
@@ -180,8 +180,8 @@ namespace MS.Internal.Text
                 textBounds = _line.GetTextBounds(cp, cch);
             }
 
-            // Pre-allocate List as we need it
             Invariant.Assert(textBounds.Count > 0);
+
             Rect[] rectangles = new Rect[textBounds.Count];
 
             for (int boundIndex = 0; boundIndex < rectangles.Length; boundIndex++)

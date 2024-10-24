9e2f0bc76866024587f365c3516658b202a60cba
h3xds1nz <gamingshard@protonmail.com>
2024-10-24 20:40:30 +0200

    Constrain CS0436 to LocalAppContext; use proper locking scheme mechanism

--- src/Microsoft.DotNet.Wpf/src/PresentationFramework/System/AppContextDefaultValues.cs
+++ src/Microsoft.DotNet.Wpf/src/PresentationFramework/System/AppContextDefaultValues.cs
@@ -2,14 +2,6 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-// Since multiple assemblies (WindowsBase, PresentationFramework, PresentationCore, PresentationBuildTasks)
-// share this file/class and have InternalsVisibleTo set between them, there will be a compiler warning
-// that the type is found both in the source and in a referenced assembly. The compiler will prefer 
-// the version of the type defined in the source once the warning is supressed and won't compile the rest.
-
-// In order to disable the warning for this type we are disabling the warning for this entire file.
-#pragma warning disable CS0436 // Type conflicts with imported type
-
 using MS.Internal;
 
 namespace System
@@ -22,6 +14,10 @@ namespace System
         /// </summary> 
         static partial void PopulateDefaultValuesPartial(string platformIdentifier, string profile, int targetFrameworkVersion)
         {
+            // When building PresentationFramework, 'LocalAppContext' from WindowsBase.dll conflicts
+            // with 'LocalAppContext' from PresentationCore.dll since there is InternalsVisibleTo set
+#pragma warning disable CS0436 // Type conflicts with imported type
+
             // The standard behavior is to draw Text/PasswordBox selections via the Adorner.
             // We want this to always be the case unless it is explicitly changed, regardless of .NET target version.
             LocalAppContext.DefineSwitchDefault(FrameworkAppContextSwitches.UseAdornerForTextboxSelectionRenderingSwitchName, true);
@@ -36,8 +32,8 @@ namespace System
             LocalAppContext.DefineSwitchDefault(FrameworkAppContextSwitches.KeyboardNavigationFromHyperlinkInItemsControlIsNotRelativeToFocusedElementSwitchName, false);
             LocalAppContext.DefineSwitchDefault(FrameworkAppContextSwitches.ItemAutomationPeerKeepsItsItemAliveSwitchName, false);
             LocalAppContext.DefineSwitchDefault(FrameworkAppContextSwitches.DisableFluentThemeWindowBackdropSwitchName, false);
+
+#pragma warning restore CS0436 // Type conflicts with imported type
         }
     }
 }
-
-#pragma warning restore CS0436 // Type conflicts with imported type

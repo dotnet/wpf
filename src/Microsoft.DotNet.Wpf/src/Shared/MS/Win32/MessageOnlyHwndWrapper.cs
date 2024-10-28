e0771e4c992cf231f76903d345132f72d2ae0dac
h3xds1nz <gamingshard@protonmail.com>
2024-10-28 15:33:27 +0100

    Use string.Empty in MessageOnlyHwndWrapper and seal it

--- src/Microsoft.DotNet.Wpf/src/Shared/MS/Win32/MessageOnlyHwndWrapper.cs
+++ src/Microsoft.DotNet.Wpf/src/Shared/MS/Win32/MessageOnlyHwndWrapper.cs
@@ -2,19 +2,14 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System;
-using System.Threading;
-using System.Security ; 
-using System.Runtime.InteropServices;
-
 namespace MS.Win32
 {
-    // Specialized version of HwndWrapper for message-only windows.
-
-    internal class MessageOnlyHwndWrapper : HwndWrapper
+    /// <summary>
+    /// Specialized version of <see cref="HwndWrapper"/> for message-only windows.
+    /// </summary>
+    internal sealed class MessageOnlyHwndWrapper : HwndWrapper
     {
-        public MessageOnlyHwndWrapper() : base(0, 0, 0, 0, 0, 0, 0, "", NativeMethods.HWND_MESSAGE)
-        {
-        }
+        public MessageOnlyHwndWrapper() : base(0, 0, 0, 0, 0, 0, 0, string.Empty, NativeMethods.HWND_MESSAGE) { }
+
     }
 }

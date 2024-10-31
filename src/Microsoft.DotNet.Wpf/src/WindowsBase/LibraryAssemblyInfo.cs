9f0027d34cfd190bf06eea524edabc4d12c982b8
Jeremy Kuhne <jeremy.kuhne@microsoft.com>
2024-10-31 13:11:43 -0700

    Make PresentationBuildTasks always AnyCPU

--- src/Microsoft.DotNet.Wpf/src/WindowsBase/LibraryAssemblyInfo.cs
+++ src/Microsoft.DotNet.Wpf/src/WindowsBase/LibraryAssemblyInfo.cs
@@ -4,4 +4,4 @@
 using System;
 using System.Runtime.CompilerServices;
 
-[assembly: InternalsVisibleTo($"WindowsBase.Tests, PublicKey={PublicKeys.Ecma}")]
+[assembly: InternalsVisibleTo("WindowsBase.Tests, PublicKey=00000000000000000400000000000000")]

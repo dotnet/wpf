24105c11fda2bc1efa7673e50fdca06c55ed9e37
h3xds1nz <gamingshard@protonmail.com>
2024-11-08 13:59:07 +0100

    Remove AppContextDefaultValues/AccessibilitySwitches values from NetFX (dead code) (#9866)
    * Remove AppContextDefaultValues/AccessibilitySwitches values from NetFX (dead code)
    
    * Constrain CS0436 to LocalAppContext; use proper locking scheme mechanism
    
    * Clean up CS0436 warning suppressions from the separate context files
    
    * Change LocalAppContext from instance/partial to static class

--- src/Microsoft.DotNet.Wpf/src/PresentationCore/System/AppContextDefaultValues.cs
+++ src/Microsoft.DotNet.Wpf/src/PresentationCore/System/AppContextDefaultValues.cs
@@ -2,50 +2,17 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System.Windows;
 using MS.Internal;
 
 namespace System
 {
-    // WPF's builds are seeing new warnings as as result of using LocalAppContext in PresentationFramework, PresentationCore and WindowsBase. 
-    // These binaries have internalsVisibleTo attribute set between them - which results in the warning. 
-    // We don't have a way of suppressing this warning effectively until the shared copies of LocalAppContext and 
-    // AppContextDefaultValues have pragmas added to suppress warning 436
-#pragma warning disable 436
     internal static partial class AppContextDefaultValues
     {
+        /// <summary>
+        /// This is a partial method. This method is responsible for populating the default values based on a TFM.
+        /// It is partial because each library should define this method in their code to contain their defaults.
+        /// </summary> 
         static partial void PopulateDefaultValuesPartial(string platformIdentifier, string profile, int targetFrameworkVersion)
-        {
-            switch (platformIdentifier)
-            {
-                case ".NETFramework":
-                    {
-                        if (targetFrameworkVersion <= 40601)
-                        {
-                            LocalAppContext.DefineSwitchDefault(CoreAppContextSwitches.DoNotScaleForDpiChangesSwitchName, true);
-                        }
-
-                        if (targetFrameworkVersion <= 40602)
-                        {
-                            LocalAppContext.DefineSwitchDefault(CoreAppContextSwitches.OverrideExceptionWithNullReferenceExceptionName, true);
-                        }
-
-                        if (targetFrameworkVersion <= 40702)
-                        {
-                            LocalAppContext.DefineSwitchDefault(CoreAppContextSwitches.DoNotUsePresentationDpiCapabilityTier2OrGreaterSwitchName, true);
-                        }
-
-                        break;
-                    }
-                case ".NETCoreApp":
-                    {
-                        InitializeNetFxSwitchDefaultsForNetCoreRuntime();
-                    }
-                    break;
-            }
-        }
-
-        private static void InitializeNetFxSwitchDefaultsForNetCoreRuntime()
         {
             LocalAppContext.DefineSwitchDefault(CoreAppContextSwitches.DoNotScaleForDpiChangesSwitchName, false);
             LocalAppContext.DefineSwitchDefault(CoreAppContextSwitches.OverrideExceptionWithNullReferenceExceptionName, false);
@@ -64,5 +31,4 @@ namespace System
             LocalAppContext.DefineSwitchDefault(CoreAppContextSwitches.DisableSpecialCharacterLigatureSwitchName, false);
         }
     }
-#pragma warning restore 436
 }
--- src/Microsoft.DotNet.Wpf/src/WindowsBase/System/Windows/AccessibilitySwitches.cs
+++ src/Microsoft.DotNet.Wpf/src/WindowsBase/System/Windows/AccessibilitySwitches.cs
@@ -28,21 +28,16 @@ namespace System.Windows
         /// <summary>
         /// This id is used by .NET to report a fatal error.
         /// </summary>
-        const int EventId = 1023;
+        private const int EventId = 1023;
 
         /// <summary>
         /// This source is used by .NET to report events.
         /// </summary>
-        const string EventSource = ".NET Runtime";
+        private const string EventSource = ".NET Runtime";
 
         #endregion
 
         #region Fields
-
-        /// <summary>
-        /// Guards against multiple definitions of default switch values.
-        /// </summary>
-        static int s_DefaultsSet = 0;
 
         /// <summary>
         /// Guards against multiple verifications of the switch values.
@@ -172,41 +167,11 @@ namespace System.Windows
         /// <param name="targetFrameworkVersion"></param>
         internal static void SetSwitchDefaults(string platformIdentifier, int targetFrameworkVersion)
         {
-            switch (platformIdentifier)
-            {
-
-                case ".NETFramework":
-                    if (Interlocked.CompareExchange(ref s_DefaultsSet, 1, 0) == 0)
-                    {
-                        if (targetFrameworkVersion <= 40700)
-                        {
-                            LocalAppContext.DefineSwitchDefault(UseLegacyAccessibilityFeaturesSwitchName, true);
-                        }
-
-                        if (targetFrameworkVersion <= 40701)
-                        {
-                            LocalAppContext.DefineSwitchDefault(UseLegacyAccessibilityFeatures2SwitchName, true);
-                        }
-
-                        if (targetFrameworkVersion <= 40702)
-                        {
-                            LocalAppContext.DefineSwitchDefault(UseLegacyAccessibilityFeatures3SwitchName, true);
-                            LocalAppContext.DefineSwitchDefault(UseLegacyToolTipDisplaySwitchName, true);
-                            LocalAppContext.DefineSwitchDefault(ItemsControlDoesNotSupportAutomationSwitchName, true);
-                        }
-                    }
-                    break;
-
-                case ".NETCoreApp":
-                    {
-                        LocalAppContext.DefineSwitchDefault(UseLegacyAccessibilityFeaturesSwitchName, false);
-                        LocalAppContext.DefineSwitchDefault(UseLegacyAccessibilityFeatures2SwitchName, false);
-                        LocalAppContext.DefineSwitchDefault(UseLegacyAccessibilityFeatures3SwitchName, false);
-                        LocalAppContext.DefineSwitchDefault(UseLegacyToolTipDisplaySwitchName, false);
-                        LocalAppContext.DefineSwitchDefault(ItemsControlDoesNotSupportAutomationSwitchName, false);
-                    }
-                    break;
-            }
+            LocalAppContext.DefineSwitchDefault(UseLegacyAccessibilityFeaturesSwitchName, false);
+            LocalAppContext.DefineSwitchDefault(UseLegacyAccessibilityFeatures2SwitchName, false);
+            LocalAppContext.DefineSwitchDefault(UseLegacyAccessibilityFeatures3SwitchName, false);
+            LocalAppContext.DefineSwitchDefault(UseLegacyToolTipDisplaySwitchName, false);
+            LocalAppContext.DefineSwitchDefault(ItemsControlDoesNotSupportAutomationSwitchName, false);
         }
 
         /// <summary>

f887fba6f248430253959f38415a7384b76807c6
ThomasGoulet73 <51839772+ThomasGoulet73@users.noreply.github.com>
2024-12-17 22:00:16 -0500

    Remove SafeReflectionInvoker Part 2 (#7485)

--- src/Microsoft.DotNet.Wpf/src/System.Xaml/System/Xaml/Schema/XamlTypeInvoker.cs
+++ src/Microsoft.DotNet.Wpf/src/System.Xaml/System/Xaml/Schema/XamlTypeInvoker.cs
@@ -24,10 +24,6 @@ namespace System.Xaml.Schema
 
         private ThreeValuedBool _isPublic;
 
-        // vvvvv---- Unused members.  Servicing policy is to retain these anyway.  -----vvvvv
-        private ThreeValuedBool _isInSystemXaml;
-        // ^^^^^----- End of unused members.  -----^^^^^
-
         protected XamlTypeInvoker()
         {
         }
@@ -222,22 +218,6 @@ namespace System.Xaml.Schema
             MethodInfo getEnumMethod = GetEnumeratorMethod();
             return (IEnumerator)getEnumMethod.Invoke(instance, s_emptyObjectArray);
         }
-
-        // vvvvv---- Unused members.  Servicing policy is to retain these anyway.  -----vvvvv
-        private bool IsInSystemXaml
-        {
-            get
-            {
-                if (_isInSystemXaml == ThreeValuedBool.NotSet)
-                {
-                    Type type = _xamlType.UnderlyingType.UnderlyingSystemType;
-                    bool result = SafeReflectionInvoker.IsInSystemXaml(type);
-                    _isInSystemXaml = result ? ThreeValuedBool.True : ThreeValuedBool.False;
-                }
-                return _isInSystemXaml == ThreeValuedBool.True;
-            }
-        }
-        // ^^^^^----- End of unused members.  -----^^^^^
 
         private bool IsPublic
         {

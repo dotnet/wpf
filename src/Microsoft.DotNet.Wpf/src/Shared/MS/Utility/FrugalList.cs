c3d9a2134cbbe12e95ef3f4d1904201f4c443fff
lindexi <lindexi_gd@163.com>
2024-12-25 16:50:07 +0800

    Reduce magic numbers in FrugalList.cs

--- src/Microsoft.DotNet.Wpf/src/Shared/MS/Utility/FrugalList.cs
+++ src/Microsoft.DotNet.Wpf/src/Shared/MS/Utility/FrugalList.cs
@@ -1,4 +1,4 @@
-// Licensed to the .NET Foundation under one or more agreements.
+﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
@@ -340,7 +340,7 @@ namespace MS.Utility
 
         public override T[] ToArray()
         {
-            T[] array = new T[1];
+            T[] array = new T[SIZE];
             array[0] = _loneEntry;
             return array;
         }

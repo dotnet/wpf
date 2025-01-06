78cce9d0faebba12b58429646c35b9cb69e1ee8c
h3xds1nz <gamingshard@protonmail.com>
2025-01-06 16:40:17 +0100

    Remove redundant Guid-init from array/alloc in NotificationWindowHelper (#9394)
    * remove Guid conversion to byte[] and additional init from byte[] when we can just copy
    
    * remove obsolete security remarks

--- src/Microsoft.DotNet.Wpf/src/PresentationCore/System/Windows/InterOp/HwndTarget.cs
+++ src/Microsoft.DotNet.Wpf/src/PresentationCore/System/Windows/InterOp/HwndTarget.cs
@@ -2519,18 +2519,8 @@ namespace System.Windows.Interop
         {
             #region Data
 
-            /// <SecurityNode>
-            ///     Critical: We dont want _notificationHwnd to be exposed and used
-            ///         by anyone besides this class.
-            /// </SecurityNode>
             private HwndWrapper _notificationHwnd; // The hwnd used to listen system wide messages
-
-            /// <SecurityNode>
-            ///     Critical: _notificationHook is the hook to listen to window
-            ///         messages. We want this to be critical that no one can get it
-            ///         listen to window messages.
-            /// </SecurityNode>
-            private HwndWrapperHook _notificationHook;
+            private HwndWrapperHook _notificationHook; // The hwnd hook to listen to window messages
 
             private int _hwndTargetCount;
             public event EventHandler<MonitorPowerEventArgs> MonitorPowerEvent;
@@ -2540,10 +2530,6 @@ namespace System.Windows.Interop
 
             #endregion
 
-            /// <SecurityNode>
-            ///     Critical: Calls critical code.
-            ///     TreatAsSafe: Doesn't expose the critical resource.
-            /// </SecurityNode>
             public NotificationWindowHelper()
             {
                 // Check for Vista or newer is needed for RegisterPowerSettingNotification.
@@ -2570,7 +2556,7 @@ namespace System.Windows.Interop
                                                 IntPtr.Zero,
                                                 wrapperHooks);
 
-                    Guid monitorGuid = new Guid(NativeMethods.GUID_MONITOR_POWER_ON.ToByteArray());
+                    Guid monitorGuid = NativeMethods.GUID_MONITOR_POWER_ON;
                     unsafe
                     {
                         _hPowerNotify = UnsafeNativeMethods.RegisterPowerSettingNotification(_notificationHwnd.Handle, &monitorGuid, 0);
@@ -2578,10 +2564,6 @@ namespace System.Windows.Interop
                 }
             }
 
-            /// <SecurityNode>
-            ///     Critical: Calls critical code.
-            ///     TreatAsSafe: Doesn't expose the critical resource.
-            /// </SecurityNode>
             public void Dispose()
             {
                 if (_hPowerNotify != IntPtr.Zero)
@@ -2601,10 +2583,6 @@ namespace System.Windows.Interop
                 }
             }
 
-            /// <SecurityNode>
-            ///     Critical: Calls critical code.
-            ///     TreatAsSafe: Doesn't expose the critical resource.
-            /// </SecurityNode>
             public void AttachHwndTarget(HwndTarget hwndTarget)
             {
                 Debug.Assert(hwndTarget != null);
@@ -2622,10 +2600,6 @@ namespace System.Windows.Interop
                 _hwndTargetCount++;
             }
 
-            /// <SecurityNode>
-            ///     Critical: Calls critical code.
-            ///     TreatAsSafe: Doesn't expose the critical resource.
-            /// </SecurityNode>
             public bool DetachHwndTarget(HwndTarget hwndTarget)
             {
                 Debug.Assert(hwndTarget != null);

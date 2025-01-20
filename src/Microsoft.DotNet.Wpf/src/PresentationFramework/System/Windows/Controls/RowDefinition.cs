412255cd023d0bc5db99c3983fb54907c03e8539
Himanshi Goyal <himgoyal@microsoft.com>
2025-01-20 17:27:58 +0530

    Grid Shorthand Syntax Implementation

--- src/Microsoft.DotNet.Wpf/src/PresentationFramework/System/Windows/Controls/RowDefinition.cs
+++ src/Microsoft.DotNet.Wpf/src/PresentationFramework/System/Windows/Controls/RowDefinition.cs
@@ -47,7 +47,7 @@ namespace System.Windows.Controls
         /// <summary>
         ///     Default ctor.
         /// </summary>
-        internal RowDefinitionCollection(Grid owner)
+        internal RowDefinitionCollection(Grid owner = null)
         {
             _owner = owner;
             PrivateOnModified();
@@ -376,6 +376,10 @@ namespace System.Windows.Controls
         {
             get
             {
+                if (_owner is null)
+                {
+                    return false;
+                }
                 return (    _owner.MeasureOverrideInProgress
                         ||  _owner.ArrangeOverrideInProgress    );
             }
@@ -389,6 +393,10 @@ namespace System.Windows.Controls
         {
             get
             {
+                if (_owner is null)
+                {
+                    return false;
+                }
                 return (    _owner.MeasureOverrideInProgress
                         ||  _owner.ArrangeOverrideInProgress    );
             }
@@ -495,6 +503,46 @@ namespace System.Windows.Controls
         #region Internal Properties
 
         /// <summary>
+        ///    Owner property.
+        /// </summary>
+        internal Grid Owner
+        {
+            get { return (_owner); }
+            set
+            {
+                if (_owner == value)
+                {
+                    return;
+                }
+
+                if (_owner is null)
+                {
+                    _owner = value;
+                    PrivateOnModified();
+                    for (int i = 0; i < _size; i++)
+                    {
+                        _owner.AddLogicalChild(_items[i]);
+                        _items[i].OnEnterParentTree();
+                    }
+                }
+                else if (value is null)
+                {
+                    PrivateOnModified();
+                    for (int i = 0; i < _size; i++)
+                    {
+                        _items[i].OnExitParentTree();
+                        _owner.RemoveLogicalChild(_items[i]);
+                    }
+                    _owner = null;
+                }
+                else
+                {
+                    throw new ArgumentException(SR.Format(SR.GridCollection_InOtherCollection, "value", "RowDefinitionCollection"));
+                }
+            }
+        }
+
+        /// <summary>
         ///     Internal version of Count.
         /// </summary>
         internal int InternalCount
@@ -587,7 +635,7 @@ namespace System.Windows.Controls
             _items[index] = value;
             value.Index = index;
 
-            _owner.AddLogicalChild(value);
+            _owner?.AddLogicalChild(value);
             value.OnEnterParentTree();
         }
 
@@ -606,7 +654,7 @@ namespace System.Windows.Controls
             _items[value.Index] = null;
             value.Index = -1;
 
-            _owner.RemoveLogicalChild(value);
+            _owner?.RemoveLogicalChild(value);
         }
 
         /// <summary>
@@ -679,8 +727,11 @@ namespace System.Windows.Controls
         private void PrivateOnModified()
         {
             _version++;
-            _owner.RowDefinitionCollectionDirty = true;
-            _owner.Invalidate();
+            if (_owner is not null)
+            {
+                _owner.RowDefinitionCollectionDirty = true;
+                _owner.Invalidate();
+            }
         }
 
         /// <summary>
@@ -714,7 +765,7 @@ namespace System.Windows.Controls
         //------------------------------------------------------
 
         #region Private Fields
-        private readonly Grid _owner;      //  owner of the collection
+        private Grid _owner;      //  owner of the collection
         private DefinitionBase[] _items;            //  storage of items
         private int _size;                          //  size of the collection
         private int _version;                       //  version tracks updates in the collection

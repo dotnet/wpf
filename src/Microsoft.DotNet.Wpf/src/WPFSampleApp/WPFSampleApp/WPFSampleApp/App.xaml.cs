294555d94495dd3f4ecc7150ad259271c6a14c8e
Harshita Mohite <v-hamohite@microsoft.com>
2025-01-28 12:39:12 +0530

    TFM Tests

---
+++ src/Microsoft.DotNet.Wpf/src/WPFSampleApp/WPFSampleApp/WPFSampleApp/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WPFSampleApp;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+

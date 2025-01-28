6311ed212c19a304fba15c75e4f78bd4521ff8ec
Harshita Mohite <v-hamohite@microsoft.com>
2025-01-28 08:46:47 +0530

    WPFSampleApp

---
+++ src/Microsoft.DotNet.Wpf/src/WPFSampleApp/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFSampleApp;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}

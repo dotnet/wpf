89432db44f4d9500945740c97dda44ef161debaf
h3xds1nz <gamingshard@protonmail.com>
2025-01-30 22:22:42 +0100

    Fix IDE0073 in UIAutomationClientSideProviders

--- src/Microsoft.DotNet.Wpf/src/UIAutomation/UIAutomationClientSideProviders/MS/Internal/AutomationProxies/CodeQuality_Approved.cs
+++ src/Microsoft.DotNet.Wpf/src/UIAutomation/UIAutomationClientSideProviders/MS/Internal/AutomationProxies/CodeQuality_Approved.cs
@@ -1,6 +1,5 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
-// See the LICENSE file in the project root for more information.
 
 using System.Diagnostics.CodeAnalysis;
 // No way to Marshal an array of SafeHandles to an array of IntPtrs for MsgWaitForMultipleObjects

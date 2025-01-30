e4faa9b44ab53d7c519bbb8fdc9142f8f8343e26
h3xds1nz <gamingshard@protonmail.com>
2025-01-30 22:22:42 +0100

    Fix IDE0073 in UIAutomationClientSideProviders

--- src/Microsoft.DotNet.Wpf/src/UIAutomation/UIAutomationClientSideProviders/MS/Internal/AutomationProxies/WindowsListViewItemStartMenu.cs
+++ src/Microsoft.DotNet.Wpf/src/UIAutomation/UIAutomationClientSideProviders/MS/Internal/AutomationProxies/WindowsListViewItemStartMenu.cs
@@ -1,6 +1,5 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
-// See the LICENSE file in the project root for more information.
 
 // Description: Win32 ListView Item proxy for the Start Menu.
 //      The Start Menu has a special use of ListViews.  The items in the

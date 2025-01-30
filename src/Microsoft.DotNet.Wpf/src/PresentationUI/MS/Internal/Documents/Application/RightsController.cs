fc901ba02968e502058db0f574e385333c4d82a2
h3xds1nz <gamingshard@protonmail.com>
2025-01-30 22:35:39 +0100

    Fix IDE0073 in PresentationUI

--- src/Microsoft.DotNet.Wpf/src/PresentationUI/MS/Internal/Documents/Application/RightsController.cs
+++ src/Microsoft.DotNet.Wpf/src/PresentationUI/MS/Internal/Documents/Application/RightsController.cs
@@ -1,6 +1,5 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
-// See the LICENSE file in the project root for more information.
 
 // Description: Responsible for the lifecycle of the RightsDocument and the actions that can
 //              be performed on it.
--- src/Microsoft.DotNet.Wpf/src/PresentationUI/MS/Internal/Documents/Application/StatusInfoItem.cs
+++ src/Microsoft.DotNet.Wpf/src/PresentationUI/MS/Internal/Documents/Application/StatusInfoItem.cs
@@ -1,6 +1,5 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
-// See the LICENSE file in the project root for more information.
 
 // Description: Used to control an (RM or DigSig) InfoBar and ToolBar item in MongooseUI
 

eec0bd0413dbf7816e732ee49251c13421630fdc
h3xds1nz <gamingshard@protonmail.com>
2025-01-30 22:39:42 +0100

    Fix IDE0073 in PresentationFramework

--- src/Microsoft.DotNet.Wpf/src/PresentationFramework/System/Windows/Data/Binding.cs
+++ src/Microsoft.DotNet.Wpf/src/PresentationFramework/System/Windows/Data/Binding.cs
@@ -1,6 +1,5 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
-// See the LICENSE file in the project root for more information.
 
 //
 // Description: Defines Binding object, which describes an instance of data Binding.

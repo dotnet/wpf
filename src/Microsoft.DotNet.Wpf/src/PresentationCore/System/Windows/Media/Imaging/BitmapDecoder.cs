dotnet/wpf

commit 3854d3238a00ceb81ad4b618686d7d5654582556

Closing the bitmapstream for UnknownBitMapDecoder (#8174)
* close the bitmapstream

* move closing stream to catch

src/Microsoft.DotNet.Wpf/src/PresentationCore/System/Windows/Media/Imaging/BitmapDecoder.cs
@@ -1190,6 +1190,7 @@ namespace System.Windows.Media.Imaging
             }
             catch
             {
+                bitmapStream.Close();
                 #pragma warning disable 6500
 
                 decoderHandle = null;

commit c2d50da690323180b7df98eae97d812704550302

Fix crash if bitmapStream is null. Fixes #10390
This fixes a regression introduced in 3854d3238a00ceb81ad4b618686d7d5654582556.

src/Microsoft.DotNet.Wpf/src/PresentationCore/System/Windows/Media/Imaging/BitmapDecoder.cs
@@ -1165,7 +1165,7 @@ namespace System.Windows.Media.Imaging
             }
             catch
             {
-                bitmapStream.Close();
+                bitmapStream?.Close();
 
                 decoderHandle = null;
                 throw;
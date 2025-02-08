c508602c2f43c4afed341633af723f320cae698b
ThomasGoulet73 <51839772+ThomasGoulet73@users.noreply.github.com>
2025-02-07 19:39:09 -0500

    PR feedback

--- src/Microsoft.DotNet.Wpf/src/WpfGfx/codegen/mcg/generators/ProtocolFingerprint.cs
+++ src/Microsoft.DotNet.Wpf/src/WpfGfx/codegen/mcg/generators/ProtocolFingerprint.cs
@@ -71,7 +71,7 @@ namespace MS.Internal.MilCodeGen.Generators
             string generatedPath = 
                 Path.Combine(
                     _resourceModel.OutputDirectory,
-                    "src\\WpfGfx\\Include"
+                    "src\\WpfGfx\\Include\\Generated"
                     );
 
             FileCodeSink cppFile = new FileCodeSink(generatedPath, "wgx_sdk_version.h");

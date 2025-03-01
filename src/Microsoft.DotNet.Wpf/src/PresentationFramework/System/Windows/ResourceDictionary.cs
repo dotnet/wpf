b42fd088ee5346d1011bf2751a9f68d377ea1de9
Bastian Schmidt <batzen@gmx.org>
2025-03-01 06:51:47 +0100

    Fixing removal

--- src/Microsoft.DotNet.Wpf/src/PresentationFramework/System/Windows/ResourceDictionary.cs
+++ src/Microsoft.DotNet.Wpf/src/PresentationFramework/System/Windows/ResourceDictionary.cs
@@ -1,4 +1,4 @@
-// Licensed to the .NET Foundation under one or more agreements.
+﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
@@ -1857,9 +1857,9 @@ namespace System.Windows
             else
             {
                 //GetWeakReferenceList(deferredResourceReference.Key)?.Remove(deferredResourceReference);
-                if (this._weakDeferredResourceReferencesMap?.TryGetValue(resourceKey, out var weakDeferredResourceReferences) is true)
+                if (this._weakDeferredResourceReferencesMap?.TryGetValue(deferredResourceReference.Key, out var weakDeferredResourceReferences) is true)
                 {
-                    weakDeferredResourceReferences.Remove(deferredResourceReference)
+                    weakDeferredResourceReferences.Remove(deferredResourceReference);
                 }
             }
         }

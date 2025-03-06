aa4abb88d4daf54717d3b576234289c214035ae9
Dipesh Kumar <dipeshkrj14@gmail.com>
2025-03-06 14:11:35 +0530

    Fixed formatting

--- src/Microsoft.DotNet.Wpf/src/WpfGfx/core/resources/lineargradient.h
+++ src/Microsoft.DotNet.Wpf/src/WpfGfx/core/resources/lineargradient.h
@@ -80,20 +80,20 @@ public:
         __in_ecount(1) const BrushContext *pBrushContext
         ) const
     {
-                // If a mapping mode is relative to the brush
-                // *or* or when a relative transform is used 
-                // *and* bounds have changed, then the realization context has changed            
+                    // If a mapping mode is relative to the brush
+                    // *or* or when a relative transform is used 
+                    // *and* bounds have changed, then the realization context has changed            
         return  (   ((m_data.m_MappingMode == MilBrushMappingMode::RelativeToBoundingBox) 
                     || (m_data.m_pRelativeTransform != NULL)) &&
 
-                // Return true if the brush sizing bounds have changed
-                //
-                // We use exact equality here because fuzzy checks are expensive, coming up 
-                // with a fuzzy threshold that defines the point at which visible changes
-                // occur isn't straightforward (i.e., the brush sizing bounds aren't
-                // in device space), and exact equality handles the case we need to optimize
-                // for where a brush fills the exact same geometry more than once.                
-                !(IsExactlyEqualRectD(pBrushContext->rcWorldBrushSizingBounds, m_cachedBrushSizingBounds) ));
+                    // Return true if the brush sizing bounds have changed
+                    //
+                    // We use exact equality here because fuzzy checks are expensive, coming up 
+                    // with a fuzzy threshold that defines the point at which visible changes
+                    // occur isn't straightforward (i.e., the brush sizing bounds aren't
+                    // in device space), and exact equality handles the case we need to optimize
+                    // for where a brush fills the exact same geometry more than once.                
+                    !(IsExactlyEqualRectD(pBrushContext->rcWorldBrushSizingBounds, m_cachedBrushSizingBounds) ));
 
     }
 

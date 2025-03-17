c4f532c89996cb2ac4653ff98246ebebb6f679eb
h3xds1nz <gamingshard@protonmail.com>
2025-03-17 11:19:50 +0100

    Fix post-merge issue

--- src/Microsoft.DotNet.Wpf/src/PresentationFramework/MS/Internal/Data/PathParser.cs
+++ src/Microsoft.DotNet.Wpf/src/PresentationFramework/MS/Internal/Data/PathParser.cs
@@ -1,4 +1,4 @@
-// Licensed to the .NET Foundation under one or more agreements.
+﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
@@ -66,8 +66,6 @@ namespace MS.Internal.Data
 
         private enum State { Init, DrillIn, Prop, Done };
         private enum IndexerState { BeginParam, ParenString, ValueString, Done }
-
-        private enum State { Init, DrillIn, Prop, Done };
 
         // Each level of the path consists of
         //      a property or indexer:

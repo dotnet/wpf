74ce469c74525717bd5ec54a451a80f12f682f30
h3xds1nz <gamingshard@protonmail.com>
2025-03-19 09:57:11 +0100

    Fix UIAutomation

--- src/Microsoft.DotNet.Wpf/src/UIAutomation/UIAutomationClient/MS/Win32/SafeNativeMethods.cs
+++ src/Microsoft.DotNet.Wpf/src/UIAutomation/UIAutomationClient/MS/Win32/SafeNativeMethods.cs
@@ -1,4 +1,4 @@
-// Licensed to the .NET Foundation under one or more agreements.
+﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
@@ -159,7 +159,7 @@ namespace MS.Win32
         public static extern int RealGetWindowClass( NativeMethods.HWND hWnd, StringBuilder classname, int nMax );
 
         [DllImport("user32.dll", CharSet=CharSet.Auto)]
-        internal extern static bool IsChild( NativeMethods.HWND parent, NativeMethods.HWND child );
+        internal static extern bool IsChild( NativeMethods.HWND parent, NativeMethods.HWND child );
 
         public const int DWMWA_CLOAKED = 14;
 
--- src/Microsoft.DotNet.Wpf/src/UIAutomation/UIAutomationClient/System/Windows/Automation/CacheRequest.cs
+++ src/Microsoft.DotNet.Wpf/src/UIAutomation/UIAutomationClient/System/Windows/Automation/CacheRequest.cs
@@ -1,4 +1,4 @@
-// Licensed to the .NET Foundation under one or more agreements.
+﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
@@ -438,7 +438,7 @@ namespace System.Windows.Automation
             return _uiaCacheRequest;
         }
 
-        static internal UiaCoreApi.UiaCacheRequest CurrentUiaCacheRequest
+        internal static UiaCoreApi.UiaCacheRequest CurrentUiaCacheRequest
         {
             get
             {

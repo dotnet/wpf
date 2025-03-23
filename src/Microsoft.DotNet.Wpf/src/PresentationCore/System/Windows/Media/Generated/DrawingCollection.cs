363fa989911be51ef9bc353040701505e74053e1
Jan Ivar Z. Carlsen <jizc@outlook.com>
2025-03-23 15:35:38 +0100

    Revert changes to generated files

--- src/Microsoft.DotNet.Wpf/src/PresentationCore/System/Windows/Media/Generated/DrawingCollection.cs
+++ src/Microsoft.DotNet.Wpf/src/PresentationCore/System/Windows/Media/Generated/DrawingCollection.cs
@@ -1,4 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
@@ -563,12 +563,18 @@ namespace System.Windows.Media
 
         private void OnInsert(object item)
         {
-            ItemInserted?.Invoke(this, item);
+            if (ItemInserted != null)
+            {
+                ItemInserted(this, item);
+            }
         }
 
         private void OnRemove(object oldValue)
         {
-            ItemRemoved?.Invoke(this, oldValue);
+            if (ItemRemoved != null)
+            {
+                ItemRemoved(this, oldValue);
+            }
         }
 
         private void OnSet(object oldValue, object newValue)

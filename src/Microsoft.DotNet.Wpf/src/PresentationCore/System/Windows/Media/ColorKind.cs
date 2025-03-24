e2e66c813807cb8126ec2f05008e4c38399fc9c4
h3xds1nz <gamingshard@protonmail.com>
2025-03-24 17:30:12 +0100

    Use file-scoped namespace for ColorKind

--- src/Microsoft.DotNet.Wpf/src/PresentationCore/System/Windows/Media/ColorKind.cs
+++ src/Microsoft.DotNet.Wpf/src/PresentationCore/System/Windows/Media/ColorKind.cs
@@ -1,33 +1,31 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
-// See the LICENSE file in the project root for more information.
 
-namespace System.Windows.Media
+namespace System.Windows.Media;
+
+/// <summary>
+/// Encapsulates color kinds as categorized via <see cref="KnownColors.MatchColor(ReadOnlySpan{char})"/>
+/// </summary>
+internal enum ColorKind
 {
     /// <summary>
-    /// Encapsulates color kinds as categorized via <see cref="KnownColors.MatchColor(ReadOnlySpan{char})"/>
+    /// Unused but it is the default value.
     /// </summary>
-    internal enum ColorKind
-    {
-        /// <summary>
-        /// Unused but it is the default value.
-        /// </summary>
-        Unknown = 0,
-        /// <summary>
-        /// Just a standard #HEXCOLOR format.
-        /// </summary>
-        NumericColor = 1,
-        /// <summary>
-        /// Color prefixed with "ContextColor ".
-        /// </summary>
-        ContextColor = 2,
-        /// <summary>
-        /// Color prefixed with "sc#".
-        /// </summary>
-        ScRgbColor = 3,
-        /// <summary>
-        /// Fallback if none of the previous ones matched.
-        /// </summary>
-        KnownColor = 4
-    }
+    Unknown = 0,
+    /// <summary>
+    /// Just a standard #HEXCOLOR format.
+    /// </summary>
+    NumericColor = 1,
+    /// <summary>
+    /// Color prefixed with "ContextColor ".
+    /// </summary>
+    ContextColor = 2,
+    /// <summary>
+    /// Color prefixed with "sc#".
+    /// </summary>
+    ScRgbColor = 3,
+    /// <summary>
+    /// Fallback if none of the previous ones matched.
+    /// </summary>
+    KnownColor = 4
 }

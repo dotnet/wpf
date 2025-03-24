531ddadf462f788c185f1750ab4bb822a68cea9d
hmishra <hmishra@microsoft.com>
2025-03-24 14:04:43 +0530

    Throw specific Win32Exception in PostMessage and move caller in try-catch

--- src/Microsoft.DotNet.Wpf/src/Shared/MS/Win32/UnsafeNativeMethodsCLR.cs
+++ src/Microsoft.DotNet.Wpf/src/Shared/MS/Win32/UnsafeNativeMethodsCLR.cs
@@ -1,4 +1,4 @@
-// Licensed to the .NET Foundation under one or more agreements.
+﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
@@ -964,7 +964,7 @@ namespace MS.Win32
         {
             if (!IntPostMessage(hwnd, msg, wparam, lparam))
             {
-                throw new Win32Exception();
+                throw new Win32Exception(Marshal.GetLastWin32Error());
             }
         }
 

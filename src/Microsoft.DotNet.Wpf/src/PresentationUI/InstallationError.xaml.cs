e207155954b9cb9c689ce75ca5ab591f1d7d624b
h3xds1nz <gamingshard@protonmail.com>
2025-03-25 17:50:53 +0100

    Fix IDE0100 in PresentationUI

--- src/Microsoft.DotNet.Wpf/src/PresentationUI/InstallationError.xaml.cs
+++ src/Microsoft.DotNet.Wpf/src/PresentationUI/InstallationError.xaml.cs
@@ -1,4 +1,4 @@
-// Licensed to the .NET Foundation under one or more agreements.
+﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
@@ -173,7 +173,7 @@ namespace Microsoft.Internal.DeploymentUI
 
         private void ShowLogFileButton()
         {
-            if (File.Exists(LogFilePath) && ErrorFlag == true)
+            if (File.Exists(LogFilePath) && ErrorFlag)
             {
                 LogFileButton.Visibility = Visibility.Visible;
                 FocusManager.SetFocusedElement(this, LogFileButton);

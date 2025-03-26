3013e884920b85572674494ac682e12c7c933932
lindexi <lindexi@users.noreply.github.com>
2025-03-26 14:25:56 +0800

    Update src/Microsoft.DotNet.Wpf/src/PresentationFramework/System/Windows/Documents/TextStore.cs
    Co-authored-by: Andrii Kurdiumov <kant2002@gmail.com>

--- src/Microsoft.DotNet.Wpf/src/PresentationFramework/System/Windows/Documents/TextStore.cs
+++ src/Microsoft.DotNet.Wpf/src/PresentationFramework/System/Windows/Documents/TextStore.cs
@@ -2916,7 +2916,7 @@ namespace System.Windows.Documents
                 var attributeVal = (UnsafeNativeMethods.TS_ATTRVAL)_preparedattributes[i];
                 if (attributeVal.val != null && attributeVal.val.vt == (short)NativeMethods.tagVT.VT_UNKNOWN && attributeVal.val.data1.Value != IntPtr.Zero)
                 {
-                    Marshal.Release(attributeVal.val.data1.Value);
+                    Marshal.Release(attributeVal.val.data1);
                 }
             }
 

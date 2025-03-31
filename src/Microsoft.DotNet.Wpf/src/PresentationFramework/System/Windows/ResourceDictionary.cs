c08649b6b7dafb3e20b89691fca00f32be38f7e7
Bastian Schmidt <batzen@gmx.org>
2025-03-31 16:24:48 +0200

    Addressing PR feedback

--- src/Microsoft.DotNet.Wpf/src/PresentationFramework/System/Windows/ResourceDictionary.cs
+++ src/Microsoft.DotNet.Wpf/src/PresentationFramework/System/Windows/ResourceDictionary.cs
@@ -1916,13 +1916,10 @@ namespace System.Windows
 
             void Inflate(DeferredResourceReference deferredResourceReference)
             {
-                if (deferredResourceReference is not null)
-                {
-                    // This will inflate the deferred reference, causing it
-                    // to be removed from the list.  The list may also be
-                    // purged of dead references.
-                    deferredResourceReference?.GetValue(BaseValueSourceInternal.Unknown);
-                }
+                // This will inflate the deferred reference, causing it
+                // to be removed from the list.  The list may also be
+                // purged of dead references.
+                deferredResourceReference?.GetValue(BaseValueSourceInternal.Unknown);
             }
         }
 

7a460243e1bbaa500164701bd724544de1916e90
h3xds1nz <gamingshard@protonmail.com>
2025-03-31 12:31:15 +0200

    Fix IDE0073 for generated files

--- src/Microsoft.DotNet.Wpf/src/WpfGfx/codegen/mcg/helpers/ManagedStyle.cs
+++ src/Microsoft.DotNet.Wpf/src/WpfGfx/codegen/mcg/helpers/ManagedStyle.cs
@@ -53,7 +53,6 @@ namespace MS.Internal.MilCodeGen.Helpers
                 [[inline]]
                     // Licensed to the .NET Foundation under one or more agreements.
                     // The .NET Foundation licenses this file to you under the MIT license.
-                    // See the LICENSE file in the project root for more information.
 
                     //
                     //

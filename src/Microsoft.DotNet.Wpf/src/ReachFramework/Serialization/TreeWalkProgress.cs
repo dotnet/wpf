93e465de8507d030d9b31504b08e6a6b42d88c60
h3xds1nz <gamingshard@protonmail.com>
2025-04-07 16:01:57 +0200

    Flatten namespace

--- src/Microsoft.DotNet.Wpf/src/ReachFramework/Serialization/TreeWalkProgress.cs
+++ src/Microsoft.DotNet.Wpf/src/ReachFramework/Serialization/TreeWalkProgress.cs
@@ -1,36 +1,29 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-/*++
-
-    Abstract:
-        This file implements the TreeWalkProgress
-        used by the Xps Serialization APIs for tracking cycles in a visual tree.
---*/
 using System.Windows.Media;
 
-namespace System.Windows.Xps.Serialization
+namespace System.Windows.Xps.Serialization;
+
+/// <summary>
+/// This class is used by the Xps Serialization APIs for tracking cycles in a visual tree.
+/// </summary>
+internal sealed class TreeWalkProgress
 {
-    /// <summary>
-    /// This class is used by the Xps Serialization APIs for tracking cycles in a visual tree.
-    /// </summary>
-    internal sealed class TreeWalkProgress
+    private readonly HashSet<ICyclicBrush> _cyclicBrushes = new();
+
+    public bool EnterTreeWalk(ICyclicBrush brush)
     {
-        private readonly HashSet<ICyclicBrush> _cyclicBrushes = new();
+        return _cyclicBrushes.Add(brush);
+    }
 
-        public bool EnterTreeWalk(ICyclicBrush brush)
-        {
-            return _cyclicBrushes.Add(brush);
-        }
+    public void ExitTreeWalk(ICyclicBrush brush)
+    {
+        _cyclicBrushes.Remove(brush);
+    }
 
-        public void ExitTreeWalk(ICyclicBrush brush)
-        {
-            _cyclicBrushes.Remove(brush);
-        }
-
-        public bool IsTreeWalkInProgress(ICyclicBrush brush)
-        {
-            return _cyclicBrushes.Contains(brush);
-        }
+    public bool IsTreeWalkInProgress(ICyclicBrush brush)
+    {
+        return _cyclicBrushes.Contains(brush);
     }
 }

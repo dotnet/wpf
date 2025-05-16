57fe8ac611226f89c1a213fea520a7a82000c6a4
h3xds1nz <gamingshard@protonmail.com>
2025-05-16 11:49:49 +0200

    Replace ArrayList in InputProcessorProfiles with fixed-size array, reduce allocations (#9946)
    * Replace ArrayList with fixed-size array of CultureInfo
    
    * Auto format, fix documentation, cleanup
    
    * Seal the class
    
    * post-merge

--- src/Microsoft.DotNet.Wpf/src/PresentationCore/System/Windows/Input/InputProcessorProfiles.cs
+++ src/Microsoft.DotNet.Wpf/src/PresentationCore/System/Windows/Input/InputProcessorProfiles.cs
@@ -2,43 +2,24 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 //
-// 
-//
 // Description: Creates ITfInputProcessorProfiles instances.
-//
 //
 
 using System.Runtime.InteropServices;
+using System.Globalization;
 using System.Threading;
 using MS.Win32;
-using System.Globalization;
-using System.Collections;
 
 namespace System.Windows.Input
 {
-    //------------------------------------------------------
-    //
-    //  InputProcessorProfiles class
-    //
-    //------------------------------------------------------
-
     /// <summary>
-    /// The InputProcessorProfiles class is always associated with 
-    /// hwndInputLanguage class.
+    /// The <see cref="InputProcessorProfiles"/> class is always associated with hwndInputLanguage class.
     /// </summary>
-    internal class InputProcessorProfiles
+    internal sealed class InputProcessorProfiles
     {
-        //------------------------------------------------------
-        //
-        //  Constructors
-        //
-        //------------------------------------------------------
-
         /// <summary>
         /// InputProcessorProfiles Constructor;
         /// </summary>
-        /// Critical - as this sets the value for _ipp.
-        /// Safe - as this just initializes it to null.
         internal InputProcessorProfiles()
         {
             // _ipp is a ValueType, hence no need for new.
@@ -46,14 +27,6 @@ namespace System.Windows.Input
             _cookie = UnsafeNativeMethods.TF_INVALID_COOKIE;
         }
 
-        //------------------------------------------------------
-        //
-        //  Internal Methods
-        //
-        //------------------------------------------------------
- 
-        #region Internal Methods
- 
         /// <summary>
         /// Initialize an interface and notify sink.
         /// </summary>
@@ -81,18 +54,10 @@ namespace System.Windows.Input
         {
             Debug.Assert(_ipp != null, "Uninitialize called without initializing");
 
-            UnadviseNotifySink();            
+            UnadviseNotifySink();
             Marshal.ReleaseComObject(_ipp);
             _ipp = null;
         }
-
-        #endregion Internal Methods
-
-        //------------------------------------------------------
-        //
-        //  Internal Properties
-        //
-        //------------------------------------------------------
 
         /// <summary>
         /// Get the current input language of the current thread.
@@ -112,7 +77,7 @@ namespace System.Windows.Input
                         IntPtr[] hklList = null;
 
                         int count = (int)SafeNativeMethods.GetKeyboardLayoutList(0, null);
-                        if (count > 1) 
+                        if (count > 1)
                         {
                             hklList = new IntPtr[count];
 
@@ -123,7 +88,7 @@ namespace System.Windows.Input
                             {
                                 if (value == (short)hklList[i])
                                 {
-                                    SafeNativeMethods.ActivateKeyboardLayout(new HandleRef(this,hklList[i]), 0);
+                                    SafeNativeMethods.ActivateKeyboardLayout(new HandleRef(this, hklList[i]), 0);
                                     break;
                                 }
                             }
@@ -134,43 +99,29 @@ namespace System.Windows.Input
         }
 
         /// <summary>
-        /// Get the list of the input languages that are available in the
-        /// current thread.
+        /// Get the list of the input languages that are available in the current thread.
         /// </summary>
-        internal ArrayList InputLanguageList
+        internal unsafe CultureInfo[] InputLanguageList
         {
-             get
-             {
-                 int nCount;
-                 IntPtr langids;
+            get
+            {
+                // ITfInputProcessorProfiles::GetLanguageList returns the pointer that was allocated by CoTaskMemAlloc().
+                _ipp.GetLanguageList(out nint ptrLanguageIDs, out int nCount);
 
-                 // ITfInputProcessorProfiles::GetLanguageList returns the pointer that was allocated by
-                 // CoTaskMemAlloc().
-                 _ipp.GetLanguageList(out langids, out nCount);
+                ReadOnlySpan<short> languageIDs = new((void*)ptrLanguageIDs, nCount);
+                CultureInfo[] langArray = new CultureInfo[nCount];
 
-                 ArrayList arrayLang = new ArrayList();
+                // Create CultureInfo from each ID and store it
+                for (int i = 0; i < langArray.Length; i++)
+                    langArray[i] = new CultureInfo(languageIDs[i]);
 
-                 for (int i = 0; i < nCount; i++)
-                 {
-                     // Unmarshal each langid from short array.
-                     short langid = Marshal.PtrToStructure<short>((IntPtr)((Int64)langids + sizeof(short) * i));
-                     arrayLang.Add(new CultureInfo(langid));
-                 }
+                // Call CoTaskMemFree().
+                Marshal.FreeCoTaskMem(ptrLanguageIDs);
 
-                 // Call CoTaskMemFree().
-                 Marshal.FreeCoTaskMem(langids);
-
-                 return arrayLang;
-             }
+                return langArray;
+            }
         }
 
-
-        //------------------------------------------------------
-        //
-        //  Private Methods
-        //
-        //------------------------------------------------------
-        
         /// <summary>
         /// This advices the input language notify sink to
         /// ITfInputProcessorProfile.
@@ -201,16 +152,14 @@ namespace System.Windows.Input
             _cookie = UnsafeNativeMethods.TF_INVALID_COOKIE;
         }
 
-        //------------------------------------------------------
-        //
-        //  Private Fields
-        //
-        //------------------------------------------------------
-                
-        // The reference to ITfInputProcessorProfile.
+        /// <summary>
+        /// The reference to <see cref="UnsafeNativeMethods.ITfInputProcessorProfiles"/>.
+        /// </summary>
         private UnsafeNativeMethods.ITfInputProcessorProfiles _ipp;
 
-        // The cookie for the advised sink.
+        /// <summary>
+        /// The cookie for the advised sink.
+        /// </summary>
         private int _cookie;
     }
 }

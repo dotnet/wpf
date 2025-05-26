d4d423038b8681aae04f78438e568502b220817f
h3xds1nz <gamingshard@protonmail.com>
2025-05-26 19:06:54 +0200

    Nullable context on WeakReferenceEnumerator, make properties readonly

--- src/Microsoft.DotNet.Wpf/src/Shared/MS/Internal/WeakReferenceEnumerator.cs
+++ src/Microsoft.DotNet.Wpf/src/Shared/MS/Internal/WeakReferenceEnumerator.cs
@@ -4,66 +4,66 @@
 using System.Collections;
 using System.Collections.ObjectModel;
 
-namespace MS.Internal
+#nullable enable
+
+namespace MS.Internal;
+
+/// <summary>
+///    This allows callers to "foreach" through a WeakReferenceList.
+///    Each weakreference is checked for liveness and "current"
+///    actually returns a strong reference to the current element.
+/// </summary>
+/// <remarks>
+///    Due to the way enumerators function, this enumerator often
+///    holds a cached strong reference to the "Current" element.
+///    This should not be a problem unless the caller stops enumerating
+///    before the end of the list AND holds the enumerator alive forever.
+/// </remarks>
+internal struct WeakReferenceListEnumerator : IEnumerator
 {
-    /// <summary>
-    ///    This allows callers to "foreach" through a WeakReferenceList.
-    ///    Each weakreference is checked for liveness and "current"
-    ///    actually returns a strong reference to the current element.
-    /// </summary>
-    /// <remarks>
-    ///    Due to the way enumerators function, this enumerator often
-    ///    holds a cached strong reference to the "Current" element.
-    ///    This should not be a problem unless the caller stops enumerating
-    ///    before the end of the list AND holds the enumerator alive forever.
-    /// </remarks>
-    internal struct WeakReferenceListEnumerator : IEnumerator
+    private readonly ReadOnlyCollection<object> _backingList;
+
+    private object? _strongReference;
+    private int _index;
+
+    public WeakReferenceListEnumerator(ReadOnlyCollection<object> backingList)
     {
-        public WeakReferenceListEnumerator( ReadOnlyCollection<object> List)
+        _index = 0;
+        _backingList = backingList;
+        _strongReference = null;
+    }
+
+    readonly object IEnumerator.Current
+    {
+        get => Current;
+    }
+
+    public readonly object Current
+    {
+        get => _strongReference ?? throw new InvalidOperationException(SR.Enumerator_VerifyContext);
+    }
+
+    public bool MoveNext()
+    {
+        object? element = null;
+
+        while (_index < _backingList.Count)
         {
-            _i = 0;
-            _List = List;
-            _StrongReference = null;
+            WeakReference weakRef = (WeakReference)_backingList[_index++];
+            element = weakRef.Target;
+            if (element is not null)
+                break;
         }
 
-        object IEnumerator.Current
-        {  get{ return Current; } }
-        
-        public object Current
-        {
-            get
-            {
-                if( null == _StrongReference )
-                {
-                    throw new System.InvalidOperationException(SR.Enumerator_VerifyContext);
-                }
-                return _StrongReference;
-            }
-        }
+        _strongReference = element;
 
-        public bool MoveNext()
-        {
-            object obj=null;
-            while( _i < _List.Count )
-            {
-                WeakReference weakRef = (WeakReference) _List[ _i++ ];
-                obj = weakRef.Target;
-                if(null != obj)
-                    break;
-            }
-            _StrongReference = obj;
-            return (null != obj);
-        }
+        return element is not null;
+    }
 
-        public void Reset()
-        {
-            _i = 0;
-            _StrongReference = null;
-        }
-
-        private int _i;
-        private ReadOnlyCollection<object> _List;
-        private object _StrongReference;
+    public void Reset()
+    {
+        _index = 0;
+        _strongReference = null;
     }
 }
 

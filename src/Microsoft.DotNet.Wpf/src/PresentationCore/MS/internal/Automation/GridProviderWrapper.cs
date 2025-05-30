f078b643957af891979ab00feb2f8e0ad6f86590
h3xds1nz <gamingshard@protonmail.com>
2025-05-30 18:58:31 +0200

    Remove using

--- src/Microsoft.DotNet.Wpf/src/PresentationCore/MS/internal/Automation/GridProviderWrapper.cs
+++ src/Microsoft.DotNet.Wpf/src/PresentationCore/MS/internal/Automation/GridProviderWrapper.cs
@@ -1,14 +1,6 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-//
-// 
-//
-// Description: Grid pattern provider wrapper for WCP
-//
-//
-
-using System.Windows.Threading;
 using System.Windows.Automation.Provider;
 using System.Windows.Automation.Peers;
 
